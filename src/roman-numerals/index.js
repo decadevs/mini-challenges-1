31b4ae150b03a13fd848046b86ba3a72c2c87ec7
Lenever <onuorah.ikechukwuvictor@gmail.com>
2020-04-22 22:30:48 +0100

    Roman numerals test passed

--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -5,6 +5,8 @@
  * @returns {number} The decimal equivalent.
  */
 function romanToDecimal(roman) {
+  var decimalNumber = 0;
+
   var romanFigures = {
     I: 1,
     V: 5,
@@ -13,9 +15,51 @@ function romanToDecimal(roman) {
     C: 100,
     D: 500,
     M: 1000,
+    IV: 4,
+    IX: 9,
+    XL: 40,
+    XC: 90,
+    CD: 400,
+    CM: 900,
   };
 
-  return romanFigures["V"];
+  // Checking and removing the double letters after adding up their roman values to output
+  if (roman.includes("IV")) {
+    decimalNumber += 4;
+    roman = roman.replace("IV", "");
+  }
+
+  if (roman.includes("IX")) {
+    decimalNumber += 9;
+    roman = roman.replace("IX", "");
+  }
+
+  if (roman.includes("XL")) {
+    decimalNumber += 40;
+    roman = roman.replace("XL", "");
+  }
+
+  if (roman.includes("XC")) {
+    decimalNumber += 90;
+    roman = roman.replace("XC", "");
+  }
+
+  if (roman.includes("CD")) {
+    decimalNumber += 400;
+    roman = roman.replace("CD", "");
+  }
+
+  if (roman.includes("CM")) {
+    decimalNumber += 900;
+    roman = roman.replace("CM", "");
+  }
+
+  // Updating the values of the rest
+  for (let letter of roman) {
+    decimalNumber += romanFigures[letter];
+  }
+
+  return decimalNumber;
 }
 
 module.exports = romanToDecimal;
