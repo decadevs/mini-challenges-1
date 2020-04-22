c5a9b1e3026f4b3a16a58d3eb5e23fdcfa20a6f6
Oladokun Oladapo <oladokun104@gmail.com>
2020-04-22 15:10:30 +0100

    Bracket Chalenge Completed

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -6,7 +6,9 @@
  */
 function isValid(str) {
   let stack = [];
+  let redundantStack = [];
 
+  // Function to check if the incoming closing bracket matches the last open bracket
   const isMatch = (open, close) => {
     if (open === "{" && close === "}") {
       return true;
@@ -15,23 +17,29 @@ function isValid(str) {
     } else if (open === "(" && close === ")") {
       return true;
     }
-
     return false;
   };
 
   const newArr = Array.from(str);
 
   newArr.forEach((i) => {
-    // Check for the opening brackets
-    if (i === "[" || (i === "{") | (i === "(")) {
+    // Check for the opening brackets and push in the stack
+    if (i === "[" || i === "{" || i === "(") {
       stack.push(i);
-    } else if (isMatch(stack[stack.length - 1], i)) {
+    }
+    // If closing bracket matches with opening bracket and remove from stackclear if true
+    else if (isMatch(stack[stack.length - 1], i)) {
       stack.pop();
+    }
+    // Push redundant brackets into redundant stack
+    else {
+      redundantStack.push(i);
     }
   });
 
   // return statement here
-  if (stack.length > 0) {
+
+  if (stack.length > 0 || redundantStack.length > 0) {
     return "invalid";
   } else {
     return "valid";
