9cb908fbe425599f47a805418447f4f16ce8bf47
adeola idowu <adeolaidowu27@gmail.com>
2020-04-22 23:11:20 +0100

    completed brackets challenge

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,26 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+  const strArr = str.split("");
+  const resultArr = [];
+  for (let i = 0; i < str.length; i++) {
+    if (strArr[i] == "{" || strArr[i] == "[" || strArr[i] == "(") {
+      resultArr.push(strArr[i]);
+    }
+    if (strArr[i] == "}" || strArr[i] == "]" || strArr[i] == ")") {
+      if (resultArr[resultArr.length - 1] == "{" && strArr[i] == "}") {
+        resultArr.pop();
+      } else if (resultArr[resultArr.length - 1] == "[" && strArr[i] == "]") {
+        resultArr.pop();
+      } else if (resultArr[resultArr.length - 1] == "(" && strArr[i] == ")") {
+        resultArr.pop();
+      } else {
+        return "invalid";
+      }
+    }
+  }
+  return resultArr.length === 0 ? "valid" : "invalid";
+}
 
 module.exports = isValid;
