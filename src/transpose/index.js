5278659667b86ff8874c894d8e50a2985a2af6d0
fredrick osuala <fredosuala@gmail.com>
2020-04-22 22:37:20 +0100

    solved and tested

--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,33 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+/**
+ * MY ALGORITHM
+ * 1. determine the rows and columns of the array
+ * 2. determine the length of the new array based on the number of columns
+ * 3. add each value of the column based on the row
+ * 
+ */
+function transpose(array) {
+    let col = array[0].length
+    let row = array.length
+    let newArr = []
+  
+  for(let i = 0; i < col; i++){
+    newArr[i] = []
+    
+    for (let j=0; j < row; j++ ){
+      newArr[i][j] = array[j][i]
+      
+    }
+    
+  }
+  return newArr
+}
 
 module.exports = transpose;
+
+console.log(transpose([
+    [1, 7, 9],
+    [2, 3, 5],
+  ]))
