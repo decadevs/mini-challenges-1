662d62c2d5d4a08f323613dae617a15e72abc82f
fredrick osuala <fredosuala@gmail.com>
2020-04-22 12:54:23 +0100

    bracket algorithms solved

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,44 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+/**
+ * MY ALGORITHM
+ * 1. create an empty stack
+ * 2. create  an obj with key value pairs made up of the various opening and closing of the brackets
+ * 3. loop through the string
+ *      1. if a particular char is an open bracket push it to the stack
+ *      2. else pop-off the last char in the stack and compare it with the current char in the loop
+ *      3. if they dont match as key value pair return 'invalid'
+ * 4. After the iteration, if there is any char left in the stack return 'invalid'
+ * 5. Else return 'valid'
+ * 
+ */
+function isValid(str) {
+      let stack = []
+    let map = {'(':')', '{':"}",'[':']'};
+
+    for(let i = 0; i < str.length; i++){
+        let char = str[i]
+        if(char === '('||char === '['||char === '{'){
+            stack.push(i)
+        }
+        else{
+            let last = stack.pop();
+            if(char !== map[str[last]]){
+                return 'invalid';
+            }
+            }
+        }
+        if(stack.length !== 0){
+           return 'invalid'
+        }
+        return 'valid';
+        
+       
+}
+       console.log(isValid("}{{{{}}}}{([])(((()))){"))
+       console.log(isValid("{{{{{{{{{{{{{}}}}}}}}}}}}}"))
+       console.log(isValid("{}{{}}{{{}}}{{{{}}}}{}{}{}"))
+       console.log(isValid("{{{{{{{{{{{{}}}}}}}}}}}()[}]"))
 
 module.exports = isValid;
