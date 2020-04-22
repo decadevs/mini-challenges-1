459aacaef1e5b2e198d9637b8fcf1b04db1ce3d6
Lenever <onuorah.ikechukwuvictor@gmail.com>
2020-04-22 23:14:05 +0100

    Bracket test almost done

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -5,9 +5,30 @@
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
 function isValid(str) {
-    var openBrackets = ["(", "[", "{"];
-    var closedBrackets = [")", "]", "}"];
+  var openBrackets = [];
+  var closedBrackets = [];
 
+  for (let bracket of str) {
+    if (bracket == "(" || bracket == "[" || bracket == "{") {
+      openBrackets.push(bracket);
+    } else {
+      closedBrackets.push(bracket);
+    }
+  }
+
+  if (
+    openBrackets.length != closedBrackets.length ||
+    str.includes("(]") ||
+    str.includes("(}") ||
+    str.includes("[)") ||
+    str.includes("[}") ||
+    str.includes("{)") ||
+    str.includes("{]")
+  ) {
+    return "invalid";
+  } else {
+    return "valid";
+  }
 }
 
 module.exports = isValid;
