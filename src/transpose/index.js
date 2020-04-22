9df9f1aa33b16bd54182baa1039e0167cdf67ffc
unknown <obrienules@gmail.com>
2020-04-22 17:14:26 +0100

    Complete transpose function and pass test

--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,20 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+    let newArr = [];
+  
+    for(let i = 0; i < array[0].length; i++){
+      newArr.push([]);
+    }; 
+     
+     array.map((arr) => {
+       arr.map((val, index) => {
+         newArr[index].push(val)
+       });
+     });
+     
+     return newArr;
+}
 
 module.exports = transpose;
