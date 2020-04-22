33d2ad8b230103779eb94ffbb6ded20fc358907d
adeola idowu <adeolaidowu27@gmail.com>
2020-04-22 21:32:09 +0100

    solved transpose algo

--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,20 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+  const rowLength = array[0].length;
+  const colLength = array.length;
+  const totalRows = [];
+  for (let k = 0; k < rowLength; k++) {
+    totalRows.push(`rowNum${k}`);
+  }
+  let output = totalRows.map((item) => (item = []));
+  for (let i = 0; i < colLength; i++) {
+    for (let j = 0; j < rowLength; j++) {
+      output[j].push(array[i][j]);
+    }
+  }
+  return output;
+}
 
 module.exports = transpose;
