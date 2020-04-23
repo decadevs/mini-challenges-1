a70facfa03ce1b33f117297d2abb657a23c82235
jbchinonso <nwekejohnbosco@gmail.com>
2020-04-23 02:18:09 +0200

    first trial code

--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -4,6 +4,33 @@
  * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
  * @returns {number} The decimal equivalent.
  */
-function romanToDecimal(roman) {}
+function romanToDecimal(roman) {
+    const romanFigure = roman.toUpperCase()
+    const romanValues = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    let sum = 0
+    for(let i = romanFigure.length - 1; i > -1; i--){
+        let tempValue
+        if(romanValues[charAt(i-1)] < romanValues[charAt(i)]){
+            tempValue = romanValues[charAt(i)] - romanValues[charAt(i-1)]
+            sum+= tempValue
+            i-=2
+        }else if(romanValues[charAt(i-1)] < romanValues[charAt(i)]){
+            sum+=romanValues[charAt(i)]
+
+        }
+    }
+
+    return sum
+
+}
 
 module.exports = romanToDecimal;
