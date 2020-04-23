b41fa78133d9a36849aa5167aae0d00ebcb94576
Kaosilisochukwu Nwizu <50839130+Kaosilisochukwu@users.noreply.github.com>
2020-04-23 13:18:55 +0100

    add my variables

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,20 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+  let cheker;
+  const myObj = {
+    "(": ")",
+    "{": "}",
+    "[": "]",
+  };
+  const values = Object.values(myObj);
+  const keys = Object.keys(myObj);
+  const ans = {
+    valid: "valid",
+    invalid: "invalid",
+  };
+  const check = () => {};
+}
 
 module.exports = isValid;
