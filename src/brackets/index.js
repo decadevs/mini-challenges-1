752eeffd16a6ac99ba91ef74282065b1aadf10a2
port2gold <kaybeebaba@gmail.com>
2020-04-23 23:58:19 +0100

    Solution to algorithm challenge

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,63 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+/*
+function isValid(str) 
+{ 
+    let brackets = "[]{}()<>";
+    let stack = [];
+
+
+    for(let bracket of input) 
+    {
+     let bracketsIndex = brackets.indexOf(bracket)   
+   
+    if(bracketsIndex % 2 ===0) 
+    {
+       stack.push(bracketsIndex + 1)
+   
+    if (stack.pop() !== bracketsIndex) 
+    {
+        return "invalid"
+    }
+   } 
+
+   }
+}
+*/
+function isValid(str) {
+
+    if (str.length <= 1)
+      return "invalid"
+    // MOB mathching opening Bracket
+    //cB closing Brackets
+    //oB Opening Brackets
+    let mOB, ch
+    let stack = []
+  
+    let oB = ['[', '{', '(']
+    let cB = [']', '}', ')']
+  
+    for (let i = 0; i < str.length; i++) {
+      ch = str[i]
+  
+      if (cB.indexOf(ch) > -1) {
+        mOB = oB[cB.indexOf(ch)]
+        if (stack.length == 0 || (stack.pop() != mOB)) {
+          return "invalid"
+        }
+      } else {
+        stack.push(ch)
+      }
+    }
+     
+    if ((stack.length == 0)==true) {
+        return "valid"
+    } else {
+        return "invalid"
+    }
+  };
+  
+  
 
 module.exports = isValid;
--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,27 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+    // find the width and height of the Array
+    var wid = array.length || 0;
+    var hei = array[0] instanceof Array ? array[0].length : 0;
+    //if the Array has zero height and width return empty array
+    if (hei ===0 || wid ===0) {
+        return [];
+    }
+    // counter i and j, transposed will be stored in trans
+    var i, j, trans=[];
+    //Loop throuth every item in the outer array(hei)
+    for (i =0; i < hei; i++) {
+        //insert a new row
+        trans[i] =[];
+        //loop through every item per item in outer array (wid)
+        for (j = 0; j < wid; j++) {
+            //save transposed data
+            trans[i][j] = array[j][i];
+        }
+    }
+    return trans;
+}
 
 module.exports = transpose;
