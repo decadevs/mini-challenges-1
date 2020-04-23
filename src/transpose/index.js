c342f1cd83a638b17915c48cb9681e93b1612779
jbchinonso <nwekejohnbosco@gmail.com>
2020-04-23 19:17:03 +0100

    first attempt completed

--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,19 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+    const newArray = []
+    for(let arr of array){
+        newArray.push([])
+    }
+
+    for( let i = 0; i < array.length; i++){
+        for(let x = 0; x < array.length; i++){
+            newArray[x].push(array[i][x])
+        }
+    }
+
+    return newArray
+}
 
 module.exports = transpose;
