6343311a06bbdc63307bfb4b6af8893e633215b8
simplyking-decadev <56389903+simplyking-decadev@users.noreply.github.com>
2020-04-23 08:15:27 +0100

    declare steps to approach problem

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -5,8 +5,18 @@
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
 function isValid(str) {
-    //declare bracket type and style
+    //Proposed step
 
+    //declare empty bracket validator array
+    //initialize loop pointer of str to 0
+    //push each str into bracket validator
+    //check current str with previous str properties that they conform with rules
+    //rules are- type must be same and current str style must be close while previous str style must be open
+    //pop out str that conform
+    //continue
+
+
+    //declare bracket type and style
     const bracket_box={
         "[":{type:"bracket",style:"open"},
         "]":{type:"bracket",style:"close"},
@@ -16,7 +26,12 @@ function isValid(str) {
         ")":{type:"parenthesis",style:"close"}
     }
 
-    
+
+ 
+
+
+
+
 }
 
 module.exports = isValid;
