90a1ce3e478eb548f7ad5641004c066cf74924cb
Kaosilisochukwu Nwizu <50839130+Kaosilisochukwu@users.noreply.github.com>
2020-04-23 22:00:19 +0100

    write solution for roman-numeral challenge

--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -5,11 +5,49 @@
  * @returns {number} The decimal equivalent.
  */
 function romanToDecimal(roman) {
-  let romanArr = roman.split('');
-  let ans = 0
+  let romanArr = roman.split("");
+  let ans = 0;
 
-  for (let i = 0; i < romanArr.length; i += 1){
-
+  for (let i = 0; i < romanArr.length; i += 1) {
+    if (romanArr[i] == "M") {
+      ans += 1000;
+    } else if (romanArr[i] == "C") {
+      if (romanArr[i + 1] == "M") {
+        ans += 900;
+        i += 1;
+      } else if (romanArr[i + 1] == "D") {
+        ans += 400;
+        i += 1;
+      } else {
+        ans += 100;
+      }
+    } else if (romanArr[i] == "D") {
+      ans += 500;
+    } else if (romanArr[i] == "X") {
+      if (romanArr[i + 1] == "L") {
+        ans += 40;
+        i += 1;
+      } else if (romanArr[i + 1] == "C") {
+        ans += 90;
+        i += 1;
+      } else {
+        ans += 10;
+      }
+    } else if (romanArr[i] == "L") {
+      ans += 50;
+    } else if (romanArr[i] == "I") {
+      if (romanArr[i + 1] == "V") {
+        ans += 4;
+        i += 1;
+      } else if (romanArr[i + 1] == "X") {
+        ans += 9;
+        i += 1;
+      } else {
+        ans += 1;
+      }
+    } else if (romanArr[i] == "V") {
+      ans += 5;
+    }
   }
   return ans;
 }
