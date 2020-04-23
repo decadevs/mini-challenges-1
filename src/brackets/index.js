1a1abf23b5818256be231a94db0467549a08acad
simplyking-decadev <56389903+simplyking-decadev@users.noreply.github.com>
2020-04-23 08:06:56 +0100

    declare bracket type and style

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,19 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+    //declare bracket type and style
+
+    const bracket_box={
+        "[":{type:"bracket",style:"open"},
+        "]":{type:"bracket",style:"close"},
+        "{":{type:"curly",style:"open"},
+        "}":{type:"curly",style:"close"},
+        "(":{type:"parenthesis",style:"open"},
+        ")":{type:"parenthesis",style:"close"}
+    }
+
+    
+}
 
 module.exports = isValid;
