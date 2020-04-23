7f44809915efca86047f1128bfe0e8318a91a039
Olatunji Oye <oyeolatunji@gmail.com>
2020-04-23 12:41:32 +0100

    transpose

--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,19 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+    let all = [];
+    let arrLen = array.length;
+    let len = array[0].length;
+    let transposed = []
+
+    for (let j = 0; j < len; j++){
+        for (let i = 0; i < arrLen; i++){
+            all.push(array[i][j])
+        }
+        transposed.push(all.splice(0, arrLen))
+    }
+    return transposed;
+}
 
 module.exports = transpose;
