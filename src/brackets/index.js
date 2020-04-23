375406ad84106a28bf6452aa8e59e9aa9aa21912
seunodewale <seunodewale.zone5@gmail.com>
2020-04-23 18:28:40 +0100

    add solution for numerals

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,18 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+  strArr = str.split();
+
+  strArr.forEach(char => {
+    count = 0;
+    for (let i=0; i<strArr.length; i++){
+      if(strArr[i] === char){
+        count++;
+      }
+    }
+  })
+
+}
 
 module.exports = isValid;
