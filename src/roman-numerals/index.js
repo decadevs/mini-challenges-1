d7e9d253d8f7d0100b56215700daa542ed53f70a
Kaosilisochukwu Nwizu <50839130+Kaosilisochukwu@users.noreply.github.com>
2020-04-23 21:18:57 +0100

    add variables for roman challenge

--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -4,6 +4,14 @@
  * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
  * @returns {number} The decimal equivalent.
  */
-function romanToDecimal(roman) {}
+function romanToDecimal(roman) {
+  let romanArr = roman.split('');
+  let ans = 0
+
+  for (let i = 0; i < romanArr.length; i += 1){
+
+  }
+  return ans;
+}
 
 module.exports = romanToDecimal;
