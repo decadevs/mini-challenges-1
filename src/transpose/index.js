6b0f8bdd128d66c39597c9dd0561ce11866ed169
simplyking-decadev <56389903+simplyking-decadev@users.noreply.github.com>
2020-04-23 19:28:25 +0100

    comment summary step to follow in transpose

--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,17 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+    //given an array A to be size nxm the transposed array B will 
+    //have number of row to be A[0].length and number of column to be
+    // A.length
+    //Having gotten row and column size of array B; 
+    //The Transposed array Bji=Aij
+
+    
+
+
+    
+}
 
 module.exports = transpose;
