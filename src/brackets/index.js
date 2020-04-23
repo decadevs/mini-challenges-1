4903328d0d5cbc34dfe5c1586189ac6502bbbf4d
Nwike Ugochukwu <bondesvick@gmail.com>
2020-04-23 15:27:13 +0100

    five cases passed on brackets with one case left to complete

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,48 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+    let box = 0, curl = 0, curve = 0;
+    let boxClose = false, curlClose = false, curveClose = false;
+
+    //str.split();
+    str.split('').forEach(element => {
+
+        if (element == "[" || element == "]") {
+
+        } else if (element == "{" || element == "}") {
+
+        } else if (element == "(" || element == ")") {
+
+        }
+
+        switch(element){
+            case "{":
+                curl +=1
+                break;
+            case "}":
+                curl -=1
+                break;
+            case "[":
+                box +=1
+                break;
+            case "]":
+                box -=1
+                break;
+            case "(":
+                curve +=1
+                break;
+            case ")":
+                curve -=1
+                break;
+        }
+    });
+
+    if (curve == 0 && box == 0 && curl == 0){
+        return "valid";
+    } else{
+        return "invalid";
+    }
+}
 
 module.exports = isValid;
