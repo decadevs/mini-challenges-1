8fb2bb2cb2f2b4cee3eb43a568558275a90331d8
Lenever <onuorah.ikechukwuvictor@gmail.com>
2020-04-24 09:56:04 +0100

    All test passed and complete

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -7,15 +7,55 @@
 function isValid(str) {
   let openBrackets = [];
   let closedBrackets = [];
-  let distinctOpenBrackets = ["(", "[", "{"];
-  let distinctClosedBrackets = [")", "]", "}"];
+  let dict = {
+    "(": ")",
+    "[": "]",
+    "{": "}",
+  };
 
+  // Seperating open and closed brackets into different arrays
   for (let bracket of str) {
     if (bracket == "(" || bracket == "[" || bracket == "{") {
       openBrackets.push(bracket);
     } else {
       closedBrackets.push(bracket);
     }
+  }
+
+  // Creating a function to check equal number of specific brackets
+  function checkError(array, arr) {
+    // Creating a character Map
+    function getCharMap(arrayy) {
+      let charMap = {};
+
+      for (let char of arrayy) {
+        if (!charMap[char]) {
+          charMap[char] = 1;
+        } else {
+          charMap[char]++;
+        }
+      }
+
+      return charMap;
+    }
+
+    let temp = [];
+
+    for (let each of array) {
+      temp.push(dict[each]);
+    }
+
+    let charMaptemp = getCharMap(temp);
+    let charMaparr = getCharMap(arr);
+
+    // Comparing character Maps
+    for (let char in charMaptemp) {
+      if (charMaptemp[char] !== charMaparr[char]) {
+        return true;
+      }
+    }
+
+    return false;
   }
 
   if (
@@ -25,7 +65,8 @@ function isValid(str) {
     str.includes("[)") ||
     str.includes("[}") ||
     str.includes("{)") ||
-    str.includes("{]")
+    str.includes("{]") ||
+    checkError(openBrackets, closedBrackets)
   ) {
     return "invalid";
   } else {
