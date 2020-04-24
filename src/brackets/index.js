a0544787fb34a10005309b685bd0463100fa48e1
unknown <leslieokoduwa@gmail.com>
2020-04-24 06:36:06 -0700

    Initial commit

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,27 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+    let openingBraces = { '{': 0, '[': 0, '(': 0, '<': 0 };
+    let closingBraces = { '}': 0, ']': 0, ')': 0, '>': 0 };
+    let bracePairs = ['{}', '[]', '()', '<>'];
+    let braceArray = [];
+    
+    for (let i = 0; i < str.length; i++) {
+        if (openingBraces[str[i]] !== undefined) {
+            braceArray.push(str[i]);
+        
+        }else if (closingBraces[str[i]] !== undefined) {
+            if (bracePairs.some(val => val === braceArray[braceArray.length - 1] + str[i])) {
+                braceArray.pop();
+            } else {
+                return "invalid";
+            }
+                
+        }
+          
+    }
+    return braceArray.length !== 0 ? 'invalid' : 'valid';
+}
 
 module.exports = isValid;
