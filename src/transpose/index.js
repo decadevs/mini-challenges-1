719adc1477c59f30739f4cccab2bd72192b12666
Adefabi Adekunle <adefabiyusuf@gmail.com>
2020-04-24 14:39:28 +0100

    commit4

--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,18 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+    let k=array.length
+ let l=array[0].length
+ let arr1=[]
+ for(var i=0;i<l;i++){
+   arr2=[]
+   for(var j=0;j<k;j++){
+   arr2.push(array[j][i])
+   }
+   arr1.push(arr2)
+ }
+ return arr1
+}
 
 module.exports = transpose;
