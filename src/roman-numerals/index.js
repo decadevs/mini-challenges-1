23cb08a9a8a08692a42fbd2b4e1f241fe672917b
faith okpala <okpalafaith1@gmail.com>
2020-04-24 15:13:00 +0100

    Solutions to challenges

--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -4,6 +4,36 @@
  * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
  * @returns {number} The decimal equivalent.
  */
-function romanToDecimal(roman) {}
+function romanToDecimal(roman) {
+    var decimal = {
+
+        M: 1000,
+        D: 500,
+        C: 100,
+        L: 50,
+        X: 10,
+        V: 5,
+        I: 1
+    };
+
+    var result = 0;
+    var input = roman.split('');
+
+    for (var i = 0; i < input.length; i++) {
+        var cur = decimal[input[i]];
+        var nex = decimal[input[i + 1]];
+
+        if (cur < nex) {
+            result += nex - cur;
+            i++;
+        } else {
+            result += cur;
+        }
+
+
+    };
+
+    return result;
+}
 
 module.exports = romanToDecimal;
--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,17 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+    const res = [];
+    for (let j = 0; j < array[0].length; j++) {
+        res[j] = new Array(array.length);
+    }
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array[0].length; j++) {
+            res[j][i] = array[i][j];
+        }
+    }
+    return res;
+}
 
 module.exports = transpose;
