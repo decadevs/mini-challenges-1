b855b39b77a0a6363909d677a11a92af9ecca4a5
Dexkode <dexkode4@gmail.com>
2020-04-24 16:55:46 +0100

    completed roman

--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -4,6 +4,23 @@
  * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
  * @returns {number} The decimal equivalent.
  */
-function romanToDecimal(roman) {}
+function romanToDecimal(roman) {
+  let rom_int = { M: 1000, D: 500, C: 100, L: 50, X: 10, V: 5, I: 1 };
+  let result = 0;
+  let values = roman
+    .toUpperCase()
+    .split("")
+    .map((el) => rom_int[el]);
+
+  for (let index = 0; index < values.length; index++) {
+    if (values[index] < values[index + 1]) {
+      result += values[index + 1] - values[index];
+      index++;
+    } else {
+      result += values[index];
+    }
+  }
+  return result;
+}
 
 module.exports = romanToDecimal;
