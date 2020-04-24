ce4c4c17ad8ea4ef64747585583ac22729192f71
morlimoore <cooliyk2@gmail.com>
2020-04-24 16:21:43 +0100

    Solved all challenges

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,33 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+    let isOpen = [];
+    let openBraces = ['(','[','{'];     //Opening braces and closing braces are hard coded 
+    let closedBraces = [')',']','}'];   //to match a uniform index in both arrays.
+    let arr = str.split('');            //split the given string into an iterable
+    if(closedBraces.includes(str[0]))   //string is invalid if it starts with a closing brace
+    return 'invalid';
+    arr.map(char => {
+        if(openBraces.includes(char)) {     //push to the isOpen array if the element
+            isOpen.push(char);              //is an opening brace
+        }
+        else {
+            if(closedBraces.indexOf(char) ==    //for a closing brace, this checks for uniformity
+            openBraces.indexOf(isOpen[isOpen.length - 1])) {    //by checking the hard coded index
+                isOpen.pop();       //of the brace against the hard coded index of the last opening
+            }                       //brace. This would let us know they compliment. Afterwards, we
+            else {                  //we pop that element if uniform, else the string is invalid.
+                return 'invalid'
+            }
+        }
+    })
+    if (!isOpen.length) {       //string is valid if all braces have been closed
+        return 'valid'
+    }
+    else {
+        return 'invalid'        //string is invalid if all braces have not been closed.
+    }
+}
 
 module.exports = isValid;
--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,19 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+    let containerArray = new Array(array.length);       //We create an empty array with number of columns =  length of
+    for(let i = 0; i < array[0].length; i++) {          // the given array to be transposed.
+        containerArray[i] = new Array(array.length);    //Next, we fill up each column of our empty array with  
+    }                                                   //smaller arrays, each with length = length of given array.
+                                                        //Number of smaller arrays = number of columns of given array
+    for(let i = 0; i < containerArray.length; i++) {
+        for(let j = 0; j < array.length; j++) {         //Here, we loop through our empty array, and the given array.
+            containerArray[i][j] = array[j][i]          //Then, we transpose the indexes of our empty array, for that
+        }                                               //of the given array, and assign the values in those indexes  
+    }                                                   //to our empty array. This gives us the transposed array.
+    
+   return containerArray;
+}
 
 module.exports = transpose;
