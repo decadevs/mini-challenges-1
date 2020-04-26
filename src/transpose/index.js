ee6b052b471e495d316806e5385fa1f0010cc6e5
anabiz <anabizconcept9@gmail.com>
2020-04-26 13:07:32 +0100

    update solution to the transpose challenge.
    finalise solution to the transpose challenge and getting the correct output.

--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,22 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+  var result = [];
+    var result1 = [];
+
+    function start(){
+        result = array.map(myFunction);
+        function myFunction(value, index, array) {
+            return value.shift();
+        }
+        return result;
+    }
+    do {
+        result1.push(start());
+        result=[];
+     }while(array[0].length>0)
+    return result1;
+}
 
 module.exports = transpose;
