efdb7ae5ea4482e74f98b19d062b523ee6e5e288
Oche Ejeh <35895581+goodnessemmanuel@users.noreply.github.com>
2020-05-01 20:36:58 +0100

    Attempted Week0 Challenge

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,44 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+    //initialisation of expected brackets
+    let storedBrackets = "()[]{}";//notice all opening bracket index are at even index
+
+    //variable that will keep records of brackets as it occurred in the user input
+    const stackBrackets = [];
+
+    //loop through the input strings i.e str for the underly operations
+    for(let eachBracket of str)
+    {
+
+        let eachBracketIndex = storedBrackets.indexOf(eachBracket);
+
+        //check if the current item of str is a bracket if not skip to the next item through the index
+        if (eachBracketIndex === -1)
+        {
+
+            continue; //jump to the next item until you get to the end of str
+
+        }
+
+        if (eachBracketIndex % 2 === 0)
+        {
+
+            stackBrackets.push(eachBracketIndex + 1);
+
+        }
+        else if (stackBrackets.pop() !== eachBracketIndex)
+        {
+
+            return 'invalid';
+
+        }
+    }
+    return stackBrackets.length === 0? 'valid' : 'invalid';//use this ternary operator to return correct option
+}
 
 module.exports = isValid;
+console.log(isValid("{}()"));//outputs valid
+console.log(isValid("}{()"));//returns invalid
+console.log(isValid("({}[])"));//returns valid
--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -4,6 +4,66 @@
  * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
  * @returns {number} The decimal equivalent.
  */
-function romanToDecimal(roman) {}
+function romanToDecimal(romanNumeral)
+{
+    //create an object with the basic roman numeral to be referenced
+    const romanDecimal = { I:1, V:5, X:10, L:50, C:100, D:500, M:1000 };
+    
+    //collect the keys i.e the roman letters and store it for reference
+    const romanKeys = Object.keys(romanDecimal);
+    
+	let resultInNumber = 0;//initialize placeholder to hold result as the conversion is  been made.
+	
+    //make sure all user input is in uppercase, if not, convert it to uppercase
+    const romanNumerals = romanNumeral.toUpperCase();
+	
+	for (let letter of romanNumerals)
+	{
+
+		if (romanKeys.indexOf(letter) === -1)
+		{
+            //if any of the letter in the input string parameter is not contained in the roman keys
+            return `invalid roman figure ${letter}`;
+            
+		}
+	}
+	
+	for (let i = 0; i < romanNumerals.length; i++)
+	{
+	
+		let num1 = romanDecimal[romanNumerals[i]];
+			
+		if ((i + 1) < romanNumerals.length)//ensure input string length is morethan the ith value before this operation
+		{
+            
+			let num2 = romanDecimal[romanNumerals[i + 1]];
+				
+			if(num1 >= num2)
+			{
+
+                resultInNumber += num1;
+                
+			}			
+			else
+			{
+                //subtract from the higer value the smaller one 
+				resultInNumber += num2 - num1;
+                i++; //start from the next inline not the current i + 1
+                
+            }
+            
+		}
+		else
+		{
+
+            resultInNumber += num1;
+            
+        }
+        
+	}
+	
+	return resultInNumber;
+
+}
 
 module.exports = romanToDecimal;
