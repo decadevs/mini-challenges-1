f5d6e29b41f0670bc1e473cca72a2fdd78e254ab
Adeyemi <ababaseun@gmail.com>
2020-05-02 14:09:06 +0100

    Solved transpose algorithim challenge

--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,48 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+  const collection = []
+
+  const column = array.length;
+  const row = array[0].length;
+
+  for (let i = 0; i < row; i++) {
+    collection.push([])
+
+  }
+
+  var i = 0;
+  var j = 0;
+  var k = 1;
+  var l = 0;
+
+
+  while (i < row) {
+    const data = Number(array[l].slice(j, k))
+    collection[j].push(data)
+
+    if (collection[j].length === column) {
+      l = 0;
+      j++;
+      k++;
+      i++;
+
+
+    } else {
+      l++;
+
+    }
+
+
+  }
+
+  return collection;
+
+}
+
+
 
 module.exports = transpose;
+
+
