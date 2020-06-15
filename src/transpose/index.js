11786b646495446a1a22e01c0b2bd6fed6ed6553
deyems <adeyemisola11@gmail.com>
2020-06-15 23:01:22 +0100

    How to transpose a matrix algorithm

--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,20 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+  if(!Array.isArray(array)) {
+    throw TypeError('You must enter an Array only');
+  }
+  const transposed = [];
+  for(let row = 0; row < array[0].length; row++){
+    let transposedRow = [];
+    for(let col = 0; col < array.length; col++){
+      let [x,y] =  [row,col];
+      transposedRow.push(array[y][x]);
+    }
+    transposed.push(transposedRow);
+  }
+  return transposed;
+}
 
 module.exports = transpose;
