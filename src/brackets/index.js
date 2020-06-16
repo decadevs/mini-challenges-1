caeeccb5de5e2a21596f5b3d214fa9ce1a343814
Adewale Nurudeen <nurudeenadeolaa@gmail.com>
2020-06-15 20:17:52 -0700

    checking for the first if statement of bracket

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,13 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+  const collections = []
+  for(let character of str) {
+    if(character === '(' || character === '{' || character === '['){
+      collections.push(character)
+    }
+  }
+}
 
 module.exports = isValid;
