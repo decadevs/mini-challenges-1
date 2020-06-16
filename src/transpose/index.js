0a9aedefefee2874d13f920739afc76a2865a16d
Abdullahi Onaolapo <onaolapo.ak@gmail.com>
2020-06-16 02:50:08 +0100

    transpose

--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,20 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+
+        let newArray = [];
+        for(let i = 0; i < array.length && i <= 9; i++){
+            newArray.push([]);
+        };
+    
+        for(let i = 0; i < array.length  && i <= 9; i++){
+            for(let j = 0; j < array[i].length && j <= 9; j++){
+                newArray[j].push(array[i][j]);
+            };
+        };
+    
+        return newArray;
+}
 
 module.exports = transpose;
