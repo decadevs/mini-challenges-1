f60255e4753bf0ddd1847f4b284aa80df2f786bd
Adewale Nurudeen <nurudeenadeolaa@gmail.com>
2020-06-15 23:53:28 -0700

    completed the whole algorithm

--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,18 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+  let copy = []
+  for(var i = 0; i < array.length; i++) {
+    for(var j = 0; j < array[i].length; j++) {
+      if(array[i][j] === undefined)
+      continue
+      if(copy[j] === undefined)
+      copy[j] = []
+      copy[j][i] = array[i][j]
+    }
+  }
+  return copy
+}
 
 module.exports = transpose;
