c4385a989a2c20fc47a71063a6767700d59fafb6
Adewale Nurudeen <nurudeenadeolaa@gmail.com>
2020-06-15 22:28:14 -0700

    working on roman to decimal

--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -4,6 +4,24 @@
  * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
  * @returns {number} The decimal equivalent.
  */
-function romanToDecimal(roman) {}
+function romanToDecimal(roman) {
+  const conversion = {
+    "M": 1000,
+    "D": 500,
+    "C": 100,
+    "L": 50,
+    "X": 10,
+    "V": 5,
+    "I": 1
+  }
+
+  let array = roman.split("")
+  let total = 0
+  for(let i = 0; i < array.length; i++) {
+    total += conversion[array[i]]
+  }
+  return total
+}
+console.log(romanToDecimal("IXXX"))
 
 module.exports = romanToDecimal;
