76503c84b31972b1bc7c654c7559c78d4f12af30
daphycisist <daphycisist@gmail.com>
2020-06-17 16:52:10 +0100

    add matrix transpose solution

--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -4,6 +4,38 @@
  * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
  * @returns {number} The decimal equivalent.
  */
-function romanToDecimal(roman) {}
+function romanToDecimal(roman) {
+  // roman = roman.split(''); roman 
+  const romanToNum = {
+    "M" : 1000,
+    "CM" : 900,
+    "D": 500,
+    "CD": 400,
+    "C": 100,
+    "XC": 90,
+    "L": 50,
+    "XL": 40,
+    "X": 10,
+    "IX": 9,
+    "V": 5,
+    "IV": 4,
+    "I": 1,
+  }
+  let sum = 0;
+  let romArr = roman.split(""); 
+  let curr, next;
+
+  for(let i=0; i < romArr.length; i++){
+    curr = romanToNum[romArr[i]]; 
+    next = romanToNum[romArr[i+1]]; 
+    
+    if(curr < next){next
+      sum-=curr; sum
+    } else {
+      sum+= curr; sum
+    }
+  }
+  return sum
+}
 
 module.exports = romanToDecimal;
--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,27 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+  let arrLength = array[0].length;
+  let newArr = [];
+
+  for(let i=0; i < arrLength; i++){
+    newArr.push([]); newArr
+  }
+
+  for (let i = 0; i < arrLength; i++) {
+    for(let j=0; j < array.length; j++){
+      newArr[i].push(array[j][i])
+    } 
+  }
+  return newArr
+}
+
+console.log(transpose([
+  [5, 12, 17, 9, 3],
+  [13, 4, 8, 14, 1],
+  [9, 6, 3, 7, 21],
+]));
+
 
 module.exports = transpose;
