42b6724dc69e66d7cf14c42becf749170c4259dd
tosincoker26 <tosincoker26@gmail.com>
2020-06-17 14:40:11 +0100

    Bracket first Attempt

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,24 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+    const match = {
+        '(':')',
+        '[':']',
+        '{':'}'
+    };
+    let stack = [];
+    for (let i = 0; i<str.length; i++){
+      if (match[s[i]]){
+        stack.push(s[i]);
+      }else{
+        let open = stack.pop();
+        if (!match[open] || match[open] !== s[i]){
+          return false;
+        }
+      }
+    }
+    return !stack.length;
+};
 
 module.exports = isValid;
