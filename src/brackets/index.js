d853142c97e68507e1fa62f04b686727dd7b59a2
daphycisist <daphycisist@gmail.com>
2020-06-17 12:12:36 +0100

    add brackets algorithm solution

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,41 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
 
+  // CREATE ARRAY OF BRACKETS
+  const bracketArr = str.split('');
+
+  //DECLARE ARRAYS OF ALL OPENING AND CLOSING BRACKET TYPES
+  const openingBrackets = ['{', '(', '['];
+  const closingBrackets = ['}', ')', ']'];
+
+  //CREATE MATCHING BRACKET PAIRS
+  const bracketMap = {
+    '{': '}',
+    '(': ')',
+    '[': ']',
+  };
+  let store = [];
+
+  //RETURN INVALID IF BRACKET ARRAY STARTS WITH A CLOSING BRACKET OR IF THE LENGTH IS ODD
+  if (closingBrackets.includes(bracketArr[0]) || bracketArr.length % 2 !== 0) {
+    return `invalid`
+  }
+
+  bracketArr.map(bracket => {
+
+    //CHECK FOR OPENING BRACKETS AND PUSH TO BRACKET STORE
+   openingBrackets.includes(bracket) ? store.push(bracket): false
+
+    // IF BRACKET IS A CLOSING BRACKET, CHECK IF IT MATCHES WITH THE LAST ELEMENT IN THE BRACKET STORE
+    closingBrackets.includes(bracket)? (bracket === bracketMap[store[store.length - 1]] ? store.pop(): false ) : false;
+  });
+
+
+//RETURN VALID IF STORE IS EMPTY ELSE RETURN INVALID
+return store.length === 0 ? `valid`: `invalid`
+};
+
+console.log(isValid('{]'))
 module.exports = isValid;
