4aee65fb104ff0aa472be17460c4d723ff731279
thoraf20 <50268671+thoraf20@users.noreply.github.com>
2020-06-18 21:16:42 +0100

    Done with week one

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,46 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+
+    if (str.length <= 1){
+        return "invalid"
+    }
+
+    let parenthesis, paren;
+    let stack = [];
+
+    let leftParen = ['[', '{', '('];
+    let rightParen = [']', '}', ')'];
+
+    for(let i = 0; i < str.length; i++){
+
+        paren = str[i];
+
+        if(leftParen.indexOf(paren) > -1){
+
+            stack.push(leftParen.indexOf(paren));
+        }
+
+            
+        else if(rightParen.indexOf(paren) > -1){
+
+            if(stack.length === 0 ) return "invalid";
+            
+            if(stack.pop() !== rightParen.indexOf(paren)) return "invalid";
+            
+        
+        }
+    }
+
+    if(stack.length !== 0){
+        return "invalid"
+
+    }
+
+    return "valid"
+
+}
+
 
 module.exports = isValid;
--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,23 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
 
-module.exports = transpose;
+    let newArray = [];
+
+    for(let row = 0; row < array.length; ++row){
+        for(let col = 0; col <array[i].length; ++col){
+
+            if(array[row][col] === undefined) continue;
+
+            if(newArray[col] === undefined) newArray[col] = [];
+            
+            newArray[col][row] = array[row][col];
+        }
+    }
+
+    return newArray;
+
+}
+
+module.exports = transpose
