0ca643762af0c1a4c060c63764473104a333db38
ndipbanyan <agnesjedidiah09@gmail.com>
2020-06-30 08:36:36 +0100

    'first commit'

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,33 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) 
+{
+    let matchingOpeningBracket, ch
+  let stack = []
+
+  let openingBrackets = ['[', '{', '(']
+  let closingBrackets = [']', '}', ')']
+
+  for (let i = 0; i < str.length; i++) 
+  {
+    ch = str[i]
+
+    if (closingBrackets.indexOf(ch) > -1) 
+    {
+      matchingOpeningBracket = openingBrackets[closingBrackets.indexOf(ch)]
+      if (stack.length == 0 || (stack.pop() != matchingOpeningBracket)) 
+      {
+        return "invalid"
+      }
+    } else {
+      stack.push(ch)
+     return "valid"
+    }
+  }
+
+  
+};
+
 
 module.exports = isValid;
--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -4,6 +4,39 @@
  * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
  * @returns {number} The decimal equivalent.
  */
-function romanToDecimal(roman) {}
+function romanToDecimal(roman) 
+{
+    //if (roman==null) return -1;
+     //roman=roman.toUpperCase;
+     const array = roman.split(''); 
+     const conversion={"M":1000, "D":500, "C":100, "L":50,"X":10,"V":5, "I":1};
+     let total=0;
+     let current;
+     let currentValue;
+     let next;
+     let nextValue;
+     for(let index=0; index < array.length; index++)
+     {
+         current=array[index];
+         currentValue=conversion[current];
+         next=array[index+1];
+         nextValue=conversion[next];
+
+         if (currentValue < nextValue )
+        {
+            total -= (currentValue);
+        }
+         else 
+        {
+            total += (currentValue);  
+        }
+         
+
+     }
+     if (total < 1 || total > 3999){
+         return "Please enter a number between 1-3999";
+         }
+      else return total;
+}
 
 module.exports = romanToDecimal;
