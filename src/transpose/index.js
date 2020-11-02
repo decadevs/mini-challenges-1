7d1c8013f9c2a08c768f1f586de9b99ab50cc280
cleveromo <clevereziogor@gmail.com>
2020-11-02 20:42:00 +0100

    creating an empty 2d array

--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,13 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+    // creating an empty 2d array
+    let newArray = [];
+    for(let i = 0; i<array.length; i++){
+        newArray.push(arrar[[]])
+    }
+    
+}
 
 module.exports = transpose;
