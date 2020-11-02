177ca35c12b15a6d06dde7814ab4536945bc3df9
cleveromo <clevereziogor@gmail.com>
2020-11-02 18:13:48 +0100

    iterate through the string and to obtain the index of each opening bracket using the indexof syntax. also to push the brackets that have even indes into result

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,21 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+    let result = [];
+    for (let i = 0; i<str.length; i++){ // iterate through the string
+        let bindx = str.indexOf(str[i]) // to obtain the index of each opening bracket
+        if(bindx % 2 === 0){
+            result.push(bindx + 1)
+        }
+        }
+    }
+}
 
 module.exports = isValid;
+
+
+// else{
+//     if(result.pop() !== bindx){
+//         return false
+//     }
