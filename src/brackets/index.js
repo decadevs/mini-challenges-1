7c3bd5fe1d32a9ac2328955d606db67ac6ebb2cb
cleveromo <clevereziogor@gmail.com>
2020-11-02 18:28:54 +0100

    to check if we cannot find a bracket that match and also return true if there is no bracket left in result else return false.

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -9,16 +9,19 @@ function isValid(str) {
     for (let i = 0; i<str.length; i++){ // iterate through the string
         let bindx = str.indexOf(str[i]) // to obtain the index of each opening bracket
         if(bindx % 2 === 0){
-            result.push(bindx + 1)
+            result.push(bindx + 1) // push the even bracket into result for comparism.
+        }else{
+            if(result.pop() !== bindx){ // to check if we cannot find a bracket that match
+                return "invalid"
+            }
         }
+        if(result.length === 0){ // return true if there is no bracket left in result else return false.
+            return true;
         }
     }
+    return false
 }
 
 module.exports = isValid;
 
 
-// else{
-//     if(result.pop() !== bindx){
-//         return false
-//     }
