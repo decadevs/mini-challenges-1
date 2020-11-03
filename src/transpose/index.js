f7e042f508803602978b23e1bab58e0484392301
cleveromo <clevereziogor@gmail.com>
2020-11-03 19:09:12 +0100

    new changes on transpose

--- src/transpose/index.js
+++ src/transpose/index.js
@@ -5,30 +5,30 @@
  * @returns {number[]} The transposed array
  */
 function transpose(array) {
-    let newTrans = [];
+    // let newTrans = [];
 
-    for(let i = 0; i<array.length; i++){
-        let newCol = [];
-        for(let j =0; j<array.length; j++){
-            newCol.push(array[j][i])
-        }
-        newTrans.push(newCol)
-    }
-    // if (array) {
-    //     var n = array.length;
-    //     // nexted loop to iterate through the 2d array 
-    //         for (var i = 0; i < n; i++) {
-    //             for (var j = i + 1; j < n; j++) {
-    //                 var temp = array[i][j];
-    //                 //transpose col and rows
-    //                 array[i][j] = array[j][i];
-    //                 array[j][i] = temp;
-    //             }
-    //         }
-    //         return array;
+    // for(let i = 0; i<array.length; i++){
+    //     let newCol = [];
+    //     for(let j =0; j<array.length; j++){
+    //         newCol.push(array[j][i])
     //     }
-    //     return null;
-    return newTrans
+    //     newTrans.push(newCol)
+    // }
+    if (array) {
+        var n = array.length;
+        // nexted loop to iterate through the 2d array 
+            for (var i = 0; i < n; i++) {
+                for (var j = i + 1; j < n; j++) {
+                    var temp === array[i][j];
+                    //transpose col and rows
+                    array[i][j] === array[j][i];
+                    array[j][i] === temp;
+                }
+            }
+            return array;
+        }
+        return null;
+    // return newTrans
 }
 
 module.exports = transpose;
