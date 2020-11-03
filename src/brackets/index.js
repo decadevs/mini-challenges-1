ec5b5be7b142e6b4a1f6f6aa2fce7fe79dd7c669
cleveromo <clevereziogor@gmail.com>
2020-11-03 22:08:11 +0100

    new change

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,52 +4,49 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-// function isValid(str) {
-    // let map = {
-    //     '(': ')',
-    //     '[': ']',
-    //     '{': '}'
-    // }
-    // let result = [];
-    // for (let i = 0; i<str.length; i++){ // iterate through the string
-    //     if(str[i]=== "(" || str[i]=== "[" || str[i]=== "{"){
-    //         result.push(str[i])
-    //     // let bindx = str.indexOf(str[i]) // to obtain the index of each opening bracket
-    //     // if(bindx % 2 === 0){
-    //     //     result.push(bindx + 1) // push the even bracket into result for comparism.
-    //     }else{
-    //         if(str[i] !== result.pop()){ // to check if we cannot find a bracket that match
-    //             return "invalid"
-    //         }
-    //     }
-    //     // if(result.length === 0){ // return true if there is no bracket left in result else return false.
-    //     //     return "valid";
-    //     // }
-    // }
-    // return (result === []) ? "valid": "invalid"
-// }
-
-const isValid = (str) => {
-    let result = [];
-    let result2 = [];
-   
-    for(let i = 0; i<str.length; i++){
-        if(str[i] === "(" || str[i] === "[" || str[i] === "{" ){
-            result.push(str[i]);
+function isValid(str) {
+    const strArr = str.split("");
+    const resultArr = [];
+    for (let i = 0; i < str.length; i++) {
+      if (strArr[i] == "{" || strArr[i] == "[" || strArr[i] == "(") {
+        resultArr.push(strArr[i]);
       }
-        if(str[i] === ")" || str[i] === "]" || str[i] === "}" ){
-            result2.push(str[i]);
-
-      }
-    return (result.length > result2.length || result.length < result2.length || result.length !== result2.length) ? "invalid" : "valid"
-    }
-    let newResult = result2.reverse();
-    for(let j = 0; j<result.length; j++){
-        if(result[j] !== newResult[j]){
-            return "invalid"
+      if (strArr[i] == "}" || strArr[i] == "]" || strArr[i] == ")") {
+        if (resultArr[resultArr.length - 1] == "{" && strArr[i] == "}") {
+          resultArr.pop();
+        } else if (resultArr[resultArr.length - 1] == "[" && strArr[i] == "]") {
+          resultArr.pop();
+        } else if (resultArr[resultArr.length - 1] == "(" && strArr[i] == ")") {
+          resultArr.pop();
+        } else {
+          return "invalid";
         }
+      }
     }
-    return "valid"
-}
+    return resultArr.length === 0 ? "valid" : "invalid";
+  }
+
+// const isValid = (str) => {
+//     let result = [];
+//     let result2 = [];
+   
+//     for(let i = 0; i<str.length; i++){
+//         if(str[i] === "(" || str[i] === "[" || str[i] === "{" ){
+//             result.push(str[i]);
+//       }
+//         if(str[i] === ")" || str[i] === "]" || str[i] === "}" ){
+//             result2.push(str[i]);
+
+//       }
+//     return (result.length > result2.length || result.length < result2.length || result.length !== result2.length) ? "invalid" : "valid"
+//     }
+//     let newResult = result2.reverse();
+//     for(let j = 0; j<result.length; j++){
+//         if(result[j] !== newResult[j]){
+//             return "invalid"
+//         }
+//     }
+//     return "valid"
+// }
 
 module.exports = isValid;
