bbec2f83fe1efb0be428f474623f552a36660efa
cleveromo <clevereziogor@gmail.com>
2020-11-03 22:37:23 +0100

    new changes

--- src/transpose/index.js
+++ src/transpose/index.js
@@ -5,34 +5,21 @@
  * @returns {number[]} The transposed array
  */
 function transpose(array) {
-    const transpose = (array) => {
-        let arr1 = []
-        for(let i = 0; i<array[0].length; i++){
-            arr2 = []
-            for(let j = 0; j<array.length; j++){
-            arr2.push(array[j][i])
-     }
-            arr1.push(arr2)
-    }
-        return arr1
-  }
+
+
+    if (array) {
+        var n = array.length;
+        // nexted loop to iterate through the 2d array 
+            for (var i = 0; i < n; i++) {
+                for (var j = i + 1; j < n; j++) {
+                    var temp = array[i][j];
+                    //transpose col and rows
+                    array[i][j] = array[j][i];
+                    array[j][i] = temp;
+                }
+            }
+            return array;
+        }
+        return null;
 }
-
-
-    // if (array) {
-    //     var n = array.length;
-    //     // nexted loop to iterate through the 2d array 
-    //         for (var i = 0; i < n; i++) {
-    //             for (var j = i + 1; j < n; j++) {
-    //                 var temp = array[i][j];
-    //                 //transpose col and rows
-    //                 array[i][j] = array[j][i];
-    //                 array[j][i] = temp;
-    //             }
-    //         }
-    //         return array;
-    //     }
-    //     return null;
-    
-
 module.exports = transpose;
