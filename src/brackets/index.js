e0651aed0ca2615a295c2a1c4a7a46bdfe5e144c
olusola <alaoolusolae@gmail.com>
2020-11-04 01:29:11 +0100

    solve brackets

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,53 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+
+        let bracket =   str.split("");
+        let open = [];
+     
+        let value = {
+             '(' : ')',
+             '[' : ']',
+             '{' : '}'
+        };
+     
+     
+             for (let index = 0; index < bracket.length; index++) {
+                 
+                 if ( bracket[index] =='(' || 
+                      bracket[index] == '{'|| 
+                      bracket[index] == '[') {
+     
+                          open.push(bracket[index]);
+     
+     
+     
+                     
+                 } else {
+     
+     
+                     let last = open.pop();
+                   
+                     if ( bracket[index] !== value[last] ){
+     
+                         return "invalid";
+                     }
+                 }
+                 
+             }
+     
+             if ( open.length !== 0 ){
+     
+                 return "invalid";
+     
+             } else {
+     
+                 return "valid";
+             }
+     
+       
+
+}
 
 module.exports = isValid;
