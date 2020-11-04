b1ac5e7189d475c5163e7afc48563008b7c5e23b
Chukwuemeka Okereke <jhayokereke@gmail.com>
2020-11-05 00:46:54 +0100

    My solutions

--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -4,6 +4,29 @@
  * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
  * @returns {number} The decimal equivalent.
  */
-function romanToDecimal(roman) {}
+function romanToDecimal(roman) {
+    let decimal = 0;
+    let map = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    for (var i = 0; i < roman.length ; i++){
+        decimal += map[roman[i]];}
+        if (roman.includes('IV') || roman.includes('IX')){
+            decimal -= 2;
+        }
+        if (roman.includes('XL') || roman.includes('XC')){
+            decimal -= 20;
+        }
+        if (roman.includes('CD') || roman.includes('CM')){
+            decimal -= 200;
+        }
+    return decimal;
+}
 
 module.exports = romanToDecimal;
--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,9 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+
+function transpose(array) {
+    return array[0].map((col, i) => array.map(row => row[i]));
+  }
 
 module.exports = transpose;
