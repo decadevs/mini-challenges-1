4aad1cde8fe012a4f27682fb0ae7056968268f2e
Anthony Benarivo <benarivo66@gmail.com>
2020-11-05 12:45:22 +0100

    iterate over the arrays

--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,13 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+    var newArr = [];
+    for (var i = 0; i < array.length; i++) {
+        for (var j = 0; j < array[i].length; j++) {
+            newArr = arr.splice([i][j], 1);
+        }
+    }
+}
 
 module.exports = transpose;
