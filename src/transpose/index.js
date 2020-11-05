daa1a3b1c227ac4f591bd478e0ab447e5aa38aec
Nkechi Emmanuel <nkechiemmanuel95@gmail.com>
2020-11-05 09:05:05 +0100

    wrote a function to transpose any 2 dimensional array

--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,15 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+  var trans = new Array (array[0].length);
+  for (var i = 0; i < trans.length; i++){
+      trans[i] = new Array(array.length);
+      for(var j = 0; j < trans[i].length; j++){
+       trans[i][j] = array[j][i];
+      }
+  }
+  return trans;
+}
 
 module.exports = transpose;
