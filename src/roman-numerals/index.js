30df38ceb35657c76e612cc9271b50123fdf3ffa
Anthony Benarivo <benarivo66@gmail.com>
2020-11-05 02:37:40 +0100

    replaced letters with equivalent numbers

--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -9,7 +9,19 @@ function romanToDecimal(roman) {
     for (var i = 0; i < arr.length; i++) {
         var item = arr[i];
         if (item === "M") {
-            item.replace
+            arr.splice(i, 1, 1000)
+        } else if (item === "D") {
+            arr.splice(i, 1, 500)
+        } else if (item === "C") {
+            arr.splice(i, 1, 100)
+        } else if (item === "L") {
+            arr.splice(i, 1, 50)
+        } else if (item === "X") {
+            arr.splice(i, 1, 10)
+        } else if (item === "V") {
+            arr.splice(i, 1, 5)
+        } else if (item === "I") {
+            arr.splice(i, 1, 1)
         }
     } 
 
