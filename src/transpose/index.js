70b93742fbd9235c4cb9db54b433d99780cab9de
ebenezer ebu <eifezulike@gmail.com>
2020-11-06 07:48:04 +0100

    made dynamic

--- src/transpose/index.js
+++ src/transpose/index.js
@@ -5,12 +5,6 @@
  * @returns {number[]} The transposed array
  */
 function transpose(array) {
-     if(array.length < 1 || array.length > 10) {
-      return;
-    } 
-    if(array[0].length < 1 || array[0].length > 10) {
-      return;
-    }
     let transposed = [];
     for(let i = 0; i < array[0].length; i++) {
       transposed[i] = [];
