66bb5d61f2b2b5eeb9428894cb4f8ad0e9bc652e
lex <Lextutor.dev@gmail.com>
2020-11-07 10:06:25 -0800

    Formatted code

--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,21 +4,19 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {let matrix = []
-      let rows = array.length
-      let columns= array[0].length
+        function transpose(array) {let matrix = []
+            let rows = array.length
+            let columns= array[0].length
 
-        for(let j =0; j< columns; j++){
-          matrix[j]= [rows]}
+                for(let j =0; j< columns; j++){
+                matrix[j]= [rows]}
 
-          for(let i =0; i< rows; i++){
-            for(let j =0; j< columns; j++){
-                matrix[j][i]= array[i][j]
-            
-          }
-      }
-        return matrix
-}
+                    for(let i =0; i< rows; i++){
+                        for(let j =0; j< columns; j++){
+                        matrix[j][i]= array[i][j]}
+                    }
+
+        return matrix}
 
 
 module.exports = transpose;
