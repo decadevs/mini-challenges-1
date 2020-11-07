f90aaceb7fa76a800d77fc244c8c182f857ea6df
ECFK75C8 <francisbnsn14@gmail.com>
2020-11-07 23:18:40 +0100

    Solved transpose

--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,16 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+    let transposedArray = [];
+    for (var i = 0; i < array[0].length; i++) {
+        var innerarray = [];
+        for (var j = 0; j < array.length; j++) {
+            innerarray.push(array[j][i]);
+        }
+        transposedArray.push(innerarray);
+    }
+    return transposedArray;
+}
 
 module.exports = transpose;
