3ee52cf5f952297bd506b06554ba38951a4ffb74
ECFK75C8 <francisbnsn14@gmail.com>
2020-11-08 19:01:15 +0100

    solved roman-numerals

--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -4,6 +4,35 @@
  * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
  * @returns {number} The decimal equivalent.
  */
-function romanToDecimal(roman) {}
+function romanToDecimal(roman) {
+    var index = 0;
+    var total = 0;
+    var specialDec = [1, 10, 100];
+    var numeralsMap = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    var romanToDecimalList = roman.split("").map((numeral)=> numeralsMap[numeral]);
+
+    while(index < romanToDecimalList.length){
+        var current = romanToDecimalList[index];
+        var next = (1 + index < romanToDecimalList.length)? romanToDecimalList[index + 1] : 0;
+        if (specialDec.includes(current) && current < next){
+            total += next - current;
+            index += 2;
+        }else{
+            total += current;
+            index += 1;
+        }
+    }
+
+    return total;
+}
 
 module.exports = romanToDecimal;
