28b1ea5dff24953cb1d0882d39e338cb9a786346
okezino <okezi002@gmail.com>
2020-11-10 14:17:33 -0800

    first solution

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,47 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+let arr = []
+let check =(arr)=>{
+    return  arr[arr.length -1]
+}
+for(let i = 0; i < str.length; i++)
+{
+
+    let initial = str.charAt(i)
+
+    if(initial === "("){
+        arr.push("(")
+    }else if( initial === ")"){
+        if(check(arr) === "("){
+            arr.pop()
+        }else{
+            return "invalid"
+        }
+    } else if(initial === "["){
+        arr.push("[")
+    }else if( initial === "]"){
+        if(check(arr) === "["){
+            arr.pop()
+        }else {
+            return "invalid"
+        }
+    } else if(initial === "{"){
+        arr.push("{")
+    }else if( initial === "}"){
+        if(check(arr) === "{"){
+            arr.pop()
+        }else{
+            return "invalid"
+        }
+    } 
+}
+if(arr.length === 0){
+    return "valid"
+}else {
+    return "invalid"
+}
+}
 
 module.exports = isValid;
