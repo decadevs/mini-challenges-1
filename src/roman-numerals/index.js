eeaf84817a73a4b6aa7b09cc41a7f82833254227
Emmanuel Macaulay <61067933+Oba-max22@users.noreply.github.com>
2021-02-25 00:18:52 +0100

    roman-numerals solved

--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -4,6 +4,22 @@
  * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
  * @returns {number} The decimal equivalent.
  */
-function romanToDecimal(roman) {}
+function romanToDecimal(roman) {
+    const hash = {'I' : 1,'V' : 5,'X' : 10,'L' : 50,'C' : 100,'D' : 500,'M' : 1000};
+    
+    let array = roman.split("");
+    let number = 0;
+
+    for (let i = 0; i < array.length; i++) {
+        let currentValue = hash[array[i]];
+        let nextValue = hash[array[i + 1]];
+        if (currentValue < nextValue) {
+            number -= currentValue;
+        } else {
+            number += currentValue;
+        }
+    }
+    return number
+}
 
 module.exports = romanToDecimal;
