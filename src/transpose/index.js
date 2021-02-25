e17bcd8937d0e9112992f3a8d3dc2d9f5b1210b8
Ekwere Diana Etuk <dianaetukekwere@gmail.com>
2021-02-25 21:59:22 +0100

    Solve transpose

--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,16 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+    const rows = array.length;
+    const cols = array[0].length;
+    const rowsT = cols;
+    const colsT = rows;
+
+    const transp = Array(rowsT).fill(0).map(x => Array(colsT).fill(0));
+    transp.forEach((x, i) => x.forEach((x2, i2) => transp[i][i2] = array[i2][i]));
+     return transp
+}
 
 module.exports = transpose;
+
