5851ef9948c2ff2890c2afd1c2a17f17db943482
toluadesola <t.toluadesola@gmail.com>
2021-02-25 10:35:02 +0100

    solved transpose

--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,24 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+
+  const rows = array.length;
+  const cols = array[0].length;
+  const newArray = [];
+
+  for (let m = 0; m < cols; m++) {
+    newArray[m] = Array(rows);
+  }
+
+  for (let n = 0; n < rows; n++) {
+    for (let m = 0; m < cols; m++) {
+      newArray[m][n] = array[n][m];
+    }
+  }
+
+  return newArray;
+
+}
 
 module.exports = transpose;
