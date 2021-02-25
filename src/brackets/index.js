4935b6752b9ab5ef829d2b69c111e69ddfdec9cb
Durodola <DURODOLA-ABDULKABIR>
2021-02-25 22:08:37 +0100

    solved brackets and transpose algorythm

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,78 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+
+function isValid(str) {
+
+    let openNormalBracket = "(";
+    let closeNormalBracket = ")";
+
+    let openCurlyBracket = "{";
+    let closeCurlyBracket = "}";
+
+    let openSquareBracket = "[";
+    let closeSquareBracket = "]";
+    let thicker = "["
+
+    let splitString = str.split("");
+
+    let countOpenNormalBracket = 0;
+    let countCloseNormalBracket = 0;
+
+    let countOpenCurlyBracket = 0;
+    let countCloseCurlyBracket = 0;
+
+    let countOpenSquareBracket = 0;
+    let countCloseSquareBracket = 0;
+
+
+
+    for(i = 0; i < splitString.length; i++){
+        if(openNormalBracket == splitString[i]){
+            countOpenNormalBracket += 1;      
+        }
+    }
+
+    for(i = 0; i < splitString.length; i++){
+        if(closeNormalBracket == splitString[i]){    
+            countCloseNormalBracket += 1;  
+        }
+    }
+
+    for(i = 0; i < splitString.length; i++){
+        if(openCurlyBracket == splitString[i]){
+            countOpenCurlyBracket += 1;      
+        }
+    }
+
+    for(i = 0; i < splitString.length; i++){
+        if(closeCurlyBracket == splitString[i]){    
+            countCloseCurlyBracket += 1;  
+        }
+    }
+    
+    for(i = 0; i < splitString.length; i++){
+        if(openSquareBracket == splitString[i]){
+            countOpenSquareBracket += 1;      
+        }
+    }
+
+    for(i = 0; i < splitString.length; i++){
+        if(closeSquareBracket == splitString[i]){    
+            countCloseSquareBracket += 1;  
+        }
+    }
+    
+
+    if( countOpenNormalBracket === countCloseNormalBracket && countOpenCurlyBracket === countCloseCurlyBracket && 
+    countOpenSquareBracket === countCloseSquareBracket ){
+        return "valid";
+    }else{
+        return "invalid";
+    }
+
+
+}
+
 
 module.exports = isValid;
--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,21 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+
+    let rows = array.length;
+    let cols = array[0].length;
+    let transpose = [];
+for ( i = 0; i<cols; i++){
+      transpose[i] = [];
+     }
+
+for(j = 0; j < rows; j++){
+  for(i = 0; i < cols; i++){
+    transpose[i][j] = array[j][i];
+  }
+}
+   return transpose;
+}
 
 module.exports = transpose;
