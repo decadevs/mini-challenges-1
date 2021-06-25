e1b60b8d3e2cb1e372c17231fc78272270e117a3
Omotosho Joseph <Joseph.omotosho98@gmail.com>
2021-06-25 19:42:35 +0100

    Mini Challenge 1

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,40 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+    // For example, `{[()[]]()}()` is valid, but `[(])` is not. Given a string, your function should determine if it is valid or not.
+    
+    let brackets = {
+    "}":"{",
+    ")":"(",
+    "]":"["
+ }
+ let closedBracketsCollection = Object.keys(brackets);//Create an array using the keys in the object
+ let openBracketsSaver = []  //create an array that will hold open brackets,parenthesis and open square brackets(comments where i use brackets i mean square,parenthesis, and braces )
+
+ for(let elem of str){//loop through the string given in the function
+     if(closedBracketsCollection.indexOf(elem) == -1 ){//if the elements in the string doesnt exist in closedBracketsCollection...
+         openBracketsSaver.push(elem)// push it to openBracketsSaver, which keeps the open brackets
+     }
+     else if(openBracketsSaver[openBracketsSaver.length-1] == brackets[elem]){//if a closed bracket is encountered.. 
+        //check the value of the closed bracket in the "brackets" object...(the value ought to be an open bracket right??)..now compare the value with the last element in the Open bracket collection.e.g the value of ")" should be "(". if the last element of the open bracket array is "(" and the value of ")" is "(" remove it from our array
+         openBracketsSaver.pop()
+
+     }else{
+         return "invalid"//if non of the conditions above is met return invalid
+     }
+ }
+ if(openBracketsSaver.length == 0){//if everything has been popped from the openBrackets then return valid
+     return "valid"
+ }else{
+     return "invalid"//if everything wasnt popped out return invalid
+ }
+
+}
+
+console.log(isValid("{[()[]]()}()"))
 
 module.exports = isValid;
+
+
+/** Always check for patterns between input and outputs to assist in creating the algorithms that will lead to the desired output*/ 
--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,27 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+    let totalArr = []// create empty
+    let i=0//initilize a while loop
+    while(totalArr.length < array[0].length){//condition to create first column
+         totalArr.push([array[0][i]])
+         i++
+    }
+    for(let i = 1 ; i<array.length;i++){//loop through remaining array that wasnt used to create the column
+        for(let j=0; j<array[i].length; j++){//loop through element in the picked array
+            totalArr[j].push(array[i][j])//push the picked element to a postion in totalArr that is in same position as the element in the chosen array
+
+        }
+    }
+     return totalArr
+}
+
+transpose([
+    [5, 12, 17, 9, 3],
+    [13, 4, 8, 14, 1],
+    [9, 6, 3, 7, 21],
+  ])
+
 
 module.exports = transpose;
