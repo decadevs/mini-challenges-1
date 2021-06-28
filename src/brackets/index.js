6329c75f060a1c5ca37f2ae4a2795c669fd55d27
Ebuka <bukasin1@gmail.com>
2021-06-28 11:44:47 +0100

    commits

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,34 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+/* function isValid(str) {}
+
+} */
+
+function isValid(str) {
+    const bracketsPair = {
+        '}':'{',
+        ')':'(',
+        ']':'['
+    }
+    const openBrackets = Object.values(bracketsPair)
+    const closeBrackets = Object.keys(bracketsPair)
+    let holdBrackets = []
+
+    for(index in str){
+       if(openBrackets.includes(str[index])){
+            holdBrackets.push(str[index])
+            console.log(index)
+        }else{
+            if(bracketsPair[str[index]] === holdBrackets[holdBrackets.length - 1]){
+                holdBrackets.pop()
+            }else{return 'invalid'}
+        
+        }
+    }
+    return holdBrackets.length === 0 ? 'valid' : 'invalid'
+}
+
+console.log(isValid(']]'))
 
 module.exports = isValid;
--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -4,6 +4,35 @@
  * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
  * @returns {number} The decimal equivalent.
  */
-function romanToDecimal(roman) {}
+/* function romanToDecimal(roman) {} */
+
+function romanToDecimal(roman){
+    roman = roman.toLowerCase()
+    const baseRomNum = {
+        i:1,
+        v:5,
+        x:10,
+        l: 50,
+        c: 100,
+        d: 500,
+        m: 1000
+    }
+    let decNum = 0
+    
+    for(i in roman){
+        let key = roman[i]
+        let preKey = roman[i-1]
+        if(baseRomNum[key] > baseRomNum[preKey]){
+            decNum += (baseRomNum[key]-2*baseRomNum[preKey])
+        }else{
+            decNum += baseRomNum[key]
+        }
+        
+    }
+
+    return decNum >= 4000 ? 'Invalid number' : decNum
+}
+
+console.log(romanToDecimal('LX'))
 
 module.exports = romanToDecimal;
