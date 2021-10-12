7593ac0170f3d694cac0001f77811b5f3339dc72
okarachidera <45210306+okarachidera@users.noreply.github.com>
2021-10-12 16:07:43 +0100

    Finished transpose question

--- src/transpose/index.js
+++ src/transpose/index.js
@@ -1,11 +1,24 @@
 /**
  * Implement the solution in this function
  *
- * @param {number[]} array The array to transpose
+ * @param {number[1,2,3]} array The array to transpose
  * @returns {number[]} The transposed array
  */
 function transpose(array) {
-    console.log(array);
+    let store=[]
+    for (let i = 0; i < array[0].length; i++) {
+        store.push([])
+    }
+    for (let i = 0; i < array[0].length; i++) {
+        for (let x = 0; x < array.length; x++) {
+            store[i].push(array[x][i])   
+            // console.log(`${array[x][i]} arrayposition=[${x}][${i}]`);     
+        }
+    
+}
+
+    // console.log(`chi answer ${store}`)
+    return store
 }
 
 module.exports = transpose;
---
+++ tesme.js
@@ -0,0 +1,54 @@
+// let store=[]
+// let array=[
+//     [5, 12, 17, 9, 3],
+//     [13, 4, 8, 14, 1],
+//     [9, 6, 3, 7, 21],
+//   ];
+
+
+// for (let i = 0; i < array[0].length; i++) {
+//         store.push([])
+// }
+// for (let i = 0; i < array[0].length; i++) {
+//     for (let x = 0; x < array.length; x++) {
+//         store[i].push(array[x][i])   
+//         console.log(`${array[x][i]} arrayposition=[${x}][${i}]`);     
+//     }
+    
+// }
+
+// console.log(store);
+// Roman numerals consist of the following symbols:
+
+// I is 1
+
+// V is 5
+
+// X is 10
+
+// L is 50
+
+// C is 100
+
+// D is 500
+
+// M is 1,000Roman numerals consist of the following symbols:
+
+// I is 1
+
+// V is 5
+
+// X is 10
+
+// L is 50
+
+// C is 100
+
+// D is 500
+
+// M is 1,000
+
+let value=98 //xcviii
+
+value=99-50
+console.log(value);
