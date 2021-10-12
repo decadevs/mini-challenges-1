805b7f9e3d9e1501fadfc1ed4be8b46c9a8051a2
Stacy250 <ogochukwuizundu23@gmail.com>
2021-10-12 18:19:37 +0100

    solution for brackets algorithm

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,31 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+  const open = [];
+
+  const bracketObj = {
+    "(" : ")",
+    "{" : "}",
+    "[" : "]"
+  }
+
+  if(Object.values(bracketObj).includes(str[0])) {
+    return 'invalid';
+  }
+  for (let i=0; i< str.length; i++){
+    if(Object.keys(bracketObj).includes(str[i])){
+      open.push(str[i])
+    } else {
+      if(bracketObj[open[open.length-1]] != str[i]) {
+        return "invalid";
+      } else {
+        open.pop()
+      }
+    }
+  }
+
+  return open.length == 0 ? 'valid' : "invalid";
+}
 
 module.exports = isValid;
