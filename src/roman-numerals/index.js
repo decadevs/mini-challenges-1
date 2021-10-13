4c4b78cece853ed3bcd5e440200653f48e2e3603
Stacy250 <ogochukwuizundu23@gmail.com>
2021-10-13 11:45:35 +0100

    transpose

--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -4,6 +4,20 @@
  * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
  * @returns {number} The decimal equivalent.
  */
-function romanToDecimal(roman) {}
+function romanToDecimal(roman) {
+  const romanNum =['M' ,'CM','D','CD ', 'C ','XC', 'L ','XL ','X ','IX','V ','IV ','I']
+  const number =[1000, 900, 500,400,100,90,50,40,10,9,5,4,1]
+  let conRoman =0;
+  for (let i=0;i< number.length; i++){
+    while(roman >= number[i]){
+    conRoman = romanNum + conRoman;
+    }
+
+  }
+ return conRoman;
+}
 
 module.exports = romanToDecimal;
+
+
+
