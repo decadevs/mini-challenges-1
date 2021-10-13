677c9e43e10035cd469dc056e493e784d3a28c5a
chidera okara <chidera.okara@decagon.dev>
2021-10-13 11:00:11 +0100

    new change

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,8 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+    
+}
 
 module.exports = isValid;
