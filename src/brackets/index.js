a8ff8196eb973bf090f34c5bd8038ddc02de5190
Abbracx <tankoraphael@gmail.com>
2021-10-13 14:49:18 +0100

    Commit for bracket

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,29 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+    
+    let myArr = [];
+    const mappedSymbols = {
+        '{': '}',
+        '[': ']',
+        '(': ')'
+    };
+
+    for(let symbol of str){
+
+        caseA = symbol === '{';
+        caseB = symbol === '[';
+        caseC = symbol === '(';
+
+        if(caseA | caseB | caseC) myArr.push(symbol);
+        else{
+            let item = myArr.pop();
+            if(mappedSymbols[item] !== symbol ) return 'invalid';
+        }
+    }
+    return (myArr.length === 0) ? 'valid':'invalid';
+
+}
 
 module.exports = isValid;
