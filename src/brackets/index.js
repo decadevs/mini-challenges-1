0093d87dfc42884e2f128fd1f7a22c7200b6dfa8
Decagon <decagon@Decagons-MacBook-Air.local>
2021-10-15 14:45:30 +0100

    test passed

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,23 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+  let stack = [];
+  let output = "";
+  str.split("").forEach((el) => {
+    if (el === "(" || el === "[" || el === "{") {
+      stack.push(el);
+    }
 
+    if (stack.length === 0) output = "invalid";
+    if (stack[stack.length - 1] === "(" && el === ")") stack.pop();
+    else if (stack[stack.length - 1] === "[" && el === "]") stack.pop();
+    else if (stack[stack.length - 1] === "{" && el === "}") stack.pop();
+    // else output = "invalid";
+  });
+
+  if (output === "invalid") return output;
+  return stack.length === 0 ? "valid" : "invalid";
+}
+console.log(isValid("(())"));
 module.exports = isValid;
--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -4,6 +4,34 @@
  * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
  * @returns {number} The decimal equivalent.
  */
-function romanToDecimal(roman) {}
+function romanToDecimal(roman) {
+  const upperCaseIn = [...roman.toUpperCase()];
+  const symbols = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  let holder = [];
+  let output = 0;
+
+  upperCaseIn.forEach((el) => {
+    holder.push(symbols[el]);
+  });
+
+  holder.forEach((el, i) => {
+    if (el >= holder[i + 1] && i < holder.length - 1) output += holder[i];
+    if (el < holder[i + 1] && i < holder.length - 1) output -= holder[i];
+    if (i === holder.length - 1) output += holder[i];
+  });
+
+  return output;
+}
 
 module.exports = romanToDecimal;
+
+// const test = numeralsToDecimal("MMCDXLVIII");
