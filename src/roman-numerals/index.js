2189b24bd7928ea45f7067c12549d9daedbf3d6e
Stacy250 <ogochukwuizundu23@gmail.com>
2021-10-15 18:09:04 +0100

    mini-challenge-1-solved

--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -1,23 +1,25 @@
-/**
- * Implement the solution in this function
- *
- * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
- * @returns {number} The decimal equivalent.
- */
-function romanToDecimal(roman) {
-  const romanNum =['M' ,'CM','D','CD ', 'C ','XC', 'L ','XL ','X ','IX','V ','IV ','I']
-  const number =[1000, 900, 500,400,100,90,50,40,10,9,5,4,1]
-  let conRoman =0;
-  for (let i=0;i< number.length; i++){
-    while(roman >= number[i]){
-    conRoman = romanNum + conRoman;
-    }
+//**Implement the solution in this function
+/*
+* @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
+* @returns {number} The decimal equivalent.
+*/
 
-  }
- return conRoman;
-}
+function romanToDecimal(roman) {
+       const romanNum = "IVXLCDM";
+       const l=[1,5,10,50,100,500,1000];
+       let sum=0;
+       while(roman){
+          if(!!roman[1] && romanNum.indexOf(roman[0]) < romanNum.indexOf(roman[1])){
+             sum += (l[romanNum.indexOf(roman[1])] - l[romanNum.indexOf(roman[0])]);
+             roman = roman.substring(2, roman.length);
+          } else {
+             sum += l[romanNum.indexOf(roman[0])];
+             roman = roman.substring(1, roman.length);
+          }
+       }
+       return sum;
+    };
 
 module.exports = romanToDecimal;
-
 
 
