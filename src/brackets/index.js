0171078f7bb8820a3a2d2e73d2396af8318cf986
Mohammed Adelabu Hassan <mohammed.hassan2011@outlook.com>
2021-10-15 16:53:51 +0100

    problem solved

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,27 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
-
-module.exports = isValid;
+ function isValid(str) {
+    const capture = [];
+    const parentObj = {
+      "(" : ")",
+      "{" : "}",
+      "[" : "]"
+    }
+    if(Object.values(parentObj).includes(str[0])) {
+      return 'invalid';
+    }
+    for (let i=0; i< str.length; i++){
+      if(Object.keys(parentObj).includes(str[i])){
+        capture.push(str[i])
+      } else {
+        if(parentObj[capture[capture.length-1]] != str[i]) {
+          return "invalid";
+        } else {
+          capture.pop()
+        }
+      }
+    }
+    return capture.length == 0 ? 'valid' : "invalid";
+  }
+  module.exports = isValid;
--- src/transpose/index.js
+++ src/transpose/index.js
@@ -1,9 +1,20 @@
 /**
  * Implement the solution in this function
  *
- * @param {number[]} array The array to transpose
+ * @param {number[1,2,3]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+ function transpose(array) {
+    let keep=[]
+    for (let i = 0; i < array[0].length; i++) {
+        keep.push([])
+    }
+    for (let i = 0; i < array[0].length; i++) {
+        for (let x = 0; x < array.length; x++) {
+            keep[i].push(array[x][i])   
+        }    
+}
+    return keep;
+}; 
 
 module.exports = transpose;
