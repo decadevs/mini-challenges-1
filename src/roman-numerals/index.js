a07c5f30d3c76a4984365851fa3bba6fcc803dcd
DolapoSoares <soaresdolapo@gmail.com>
2021-10-15 15:12:12 +0100

    Solved the problems and all test passed

--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -4,6 +4,33 @@
  * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
  * @returns {number} The decimal equivalent.
  */
-function romanToDecimal(roman) {}
+function romanToDecimal(roman) {
+    let romanObj = {
+        "I" : 1,
+        "V" : 5,
+        "X" : 10,
+        "L" : 50,
+        "C" : 100,
+        "D" : 500,
+        "M" : 1000
+    }
 
+let newRoman = roman.split("");
+let number = 0;
+
+for(let i = 0; i < newRoman.length; i++){
+    let current = newRoman[i];
+    let currentValue = romanObj[current];
+    let next = newRoman[i + 1];
+    let nextValue = romanObj[next]
+
+    if(currentValue < nextValue){
+        number -= currentValue;
+    }else{
+        number += currentValue
+    }
+}
+
+return number
+}
 module.exports = romanToDecimal;
