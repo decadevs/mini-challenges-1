70aefdea9f7d20473a52da347c17f04604eade23
DhanteyUD <otseclinton60@gmail.com>
2022-01-30 21:05:37 +0100

    Initial commit

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,22 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+
+function isValid(str) {
+  let store = [];
+  if(str.length % 2 !== 0) return "invalid";
+  for(let char of str) {
+    if(char === "(" || char === "{" || char === "[") {  // If character is an opening bracket...
+      store.push(char)
+    } else if((char === ")" && store[store.length - 1] === "(") ||
+              (char === "}" && store[store.length - 1] === "{") ||
+              (char === "]" && store[store.length - 1] === "[")) { // If character is a closing bracket and last item stored is a matching open pair...
+      store.pop()
+    } else return "invalid"
+
+  }
+  return store.length ? "invalid" : "valid"
+
+}
 
 module.exports = isValid;
--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -4,6 +4,34 @@
  * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
  * @returns {number} The decimal equivalent.
  */
-function romanToDecimal(roman) {}
+
+function romanToDecimal(roman) {
+  let total = 0;
+  const converter = {
+    I : 1,
+    V : 5,
+    X : 10,
+    L : 50,
+    C : 100,
+    D : 500,
+    M : 1000
+  };
+  for (let i=0; i < roman.length; i++) {
+    let current = converter[roman.charAt(i)];
+    let next = converter[roman.charAt(i+1)];
+    if (next) { // If there is a next character after the current character
+      if (current >= next) {
+        total += current;
+      } else {
+        total += (next - current);
+        i++;
+      }
+    } else { // If the current character is the last chaaracter
+      total += current
+    }
+  }
+  return total;
+}
+
 
 module.exports = romanToDecimal;
