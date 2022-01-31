6d48d3d31ce2ee6b58dfac2bcf652f7822134b01
Esmart234 <esmart234@gmail.com>
2022-01-31 08:01:28 +0800

    Solved Mini-Challenge-1

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,31 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+    let stack = [];
+    let ans = "valid";
+    const brackets = {
+        "(": ")",
+        "[": "]",
+        "{": "}"
+    }
+    if (!str || str.length % 2 !== 0) {
+        return "invalid"
+    }
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] in brackets) {
+            // Check if it is an open bracket
+            stack.push(str[i]);
+        } else if (stack.length > 0 && str[i] === brackets[stack[stack.length - 1]]) {
+            // Check if the closing bracket is not starting the string
+            // And check if the current item closes with value in the brackets
+            stack.pop();
+        } else { ans = "invalid" }
+    }
+    if (stack.length === str.length) {
+        // To confirm if all input are not open bracket
+        return "invalid"
+    } else return ans
+}
 
 module.exports = isValid;
--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,20 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+    let rowNum = array[0].length;
+    let colNum = array.length;
+
+    const outterArray = [];
+    for (let row = 0; row < rowNum; row++) {
+        let innerArray = []
+        for (let col = 0; col < colNum; col++) {
+            // Inner loop that takes the nth(row) of each columns 
+            innerArray.push(array[col][row])
+        }
+        outterArray.push(innerArray)
+    }
+    return outterArray
+}
 
 module.exports = transpose;
