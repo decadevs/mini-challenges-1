83e75543037a106b11097b61b78e8ef5258e5954
MAC <mac@MACs-MacBook-Air.local>
2022-01-31 05:56:23 -0800

    Added brackets and transpose

--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,24 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+
+function transpose(array) {
+    const emptyArr = [];
+    for (let i = 0; i < array.length; i++) {
+        const hold = [];
+
+        for (let j = 0; j < array.length; j++) {
+            hold.push(array[j][i]);
+        }
+        emptyArr.push(hold);
+    }
+    return emptyArr;
+}
+console.log(transpose([
+    [9, 1, 2, 3],
+    [4, 5, 6, 7],
+    [8, 9, 10, 7],
+    [1, 3, 1, 5],
+  ]));
 
 module.exports = transpose;
