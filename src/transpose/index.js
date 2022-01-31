e2706706fd31dc46205e2427889ba55002629954
Admolgit <zealous890@gmail.com>
2022-01-31 22:03:46 +0100

    transpose-solution

--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,29 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+  // Change the array using object.key
+  let ArrObj = Object.keys(array[0]);
+
+  // loop through the array object using high order array method (choose the column first)
+  let ArrObjMap = ArrObj.map(col => {
+
+    // loop through again with map (this time using the row)
+    let colMap = array.map(row => {
+
+      // Change the row to col and vice versa.
+      let colRow = row[col];
+
+      // Return the row and column
+      return colRow;
+    });
+
+    // Return the col map
+    return colMap;
+  });
+
+  // Return array object map
+  return ArrObjMap;
+}
 
 module.exports = transpose;
