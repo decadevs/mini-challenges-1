e74dde906250bf93573e36d4fc79bb78b4cad5f2
Gboyega Lawal <afiz.lawal51@gmail.com>
2022-01-31 13:27:53 +0100

    challenge-1 complete

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,25 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+ function isValid(str) {
+   
+    let  stack = []
+    if(str.length % 2 !== 0) return "invalid"
+     
+    for(let char of str) {
+        if(char === "(" || char === "{" || char === "[") {
+            stack.push(char)
+            
+        } else if((char === ")" && stack[stack.length - 1] === "(") || (char === "}" && stack[stack.length - 1] === "{") || (char === "]" && stack[stack.length - 1] === "[")) {
+            
+            stack.pop()
+        } else return "invalid"
+    }
+    
+   
+     return stack.length ? "invalid" : "valid"
+    
+    
+}
 
 module.exports = isValid;
