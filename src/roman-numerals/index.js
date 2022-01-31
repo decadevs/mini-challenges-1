18990f264003b67c1d988f7ef5eb31d87d42c40a
stevechude <stevechude@gmail.com>
2022-01-31 14:43:02 -0800

    mini challenge 1 solution

--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -4,6 +4,20 @@
  * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
  * @returns {number} The decimal equivalent.
  */
-function romanToDecimal(roman) {}
+function romanToDecimal(n) {
+    let obj = {I:1, V:5, X:10, L:50, C:100, D:500, M:1000}
+    let result = [];
+
+    for (let i = 0; i < n.length; i++) {
+        if (obj[n[i]] < obj[n[i+1]]) {
+            result.push(obj[n[i+1]] - obj[n[i]]);
+            i++;
+            //continue;
+        } else {
+            result.push(obj[n[i]]);
+        }
+    }
+    return result.reduce((a,b) => a + b, 0);
+}
 
 module.exports = romanToDecimal;
