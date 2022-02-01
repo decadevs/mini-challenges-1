12ac86ba99e8ec205cc694ec295851712858edec
Decagon <decagon@Decagons-MacBook-Air.local>
2022-02-01 05:39:49 +0100

    Challenges passed on Testing

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,36 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+
+function isValid(str) {
+  const bracketArray = str.split(""); // Converting the input String of brackets to a new Array of brackets
+
+  if (bracketArray[0] === "}" || bracketArray[0] === "]" || bracketArray[0] === ")") {
+    // Checking for wrong bracket input
+    return "invalid";
+  }
+
+  let newArray = [];  // Initializing a new Array to hold the valid brakcet inputs
+
+  for (let bracket = 0; bracket < bracketArray.length; bracket++) {    // Looping through the Bracket Array
+    if (bracketArray[bracket] === "[" || bracketArray[bracket] === "(" || bracketArray[bracket] === "{") {
+      newArray.push(bracketArray[bracket]);
+    }
+
+    if (newArray[newArray.length - 1] === "(" && bracketArray[bracket] === ")") {
+      newArray.pop();
+    } else if (newArray[newArray.length - 1] === "[" && bracketArray[bracket] === "]") {
+      newArray.pop();
+    } else if (newArray[newArray.length - 1] === "{" && bracketArray[bracket] === "}") {
+      newArray.pop();
+    }
+  }
+
+  if (newArray.length === 0) {
+    return "valid";
+  } else {
+    return "invalid";
+  }
+}
 
 module.exports = isValid;
--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -4,6 +4,46 @@
  * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
  * @returns {number} The decimal equivalent.
  */
-function romanToDecimal(roman) {}
+
+const romanHash = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000,
+};
+
+function romanToDecimal(str) {
+  let total = 0;
+  for (let j = 0; j < str.length; j++) {
+    if (str[j] === "I" && str[j + 1] === "V") {
+      total += 4;
+      j++;
+    } else if (str[j] === "I" && str[j + 1] === "X") {
+      total += 9;
+      j++;
+    } else if (str[j] === "X" && str[j + 1] === "L") {
+      total += 40;
+      j++;
+    } else if (str[j] === "X" && str[j + 1] === "C") {
+      total += 90;
+      j++;
+    } else if (str[j] === "C" && str[j + 1] === "D") {
+      total += 400;
+      j++;
+    } else if (str[j] === "C" && str[j + 1] === "M") {
+      total += 900;
+      j++;
+    } else {
+      total += romanHash[str[j]];
+    }
+  }
+  return total;
+}
+
+console.log(romanToDecimal('CL'));
+
 
 module.exports = romanToDecimal;
