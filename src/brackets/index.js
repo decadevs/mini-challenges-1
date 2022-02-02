98138ecc0c7eb34acd23429e2c6efa9bee10aca5
adefoluso <adedejifoluso@gmail.com>
2022-02-02 08:19:10 +0100

    submission for mini challenges 1

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,42 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+
+function isValid(str) {
+  let open = []; //creates an empty array
+
+  if (str.length % 2 !== 0) {
+    //This checks for the length of bracket character passed in, an odd number means no matching bracket
+    return "invalid";
+  }
+
+  const openBrackets = new Set(["(", "[", "{"]); // a new instance of the acceptable brackets is created
+
+  const bracketCorrespondence = {
+    //acceptable corresponding matching closing brackets in key value pairs
+    ")": "(",
+    "]": "[",
+    "}": "{",
+  };
+
+  for (let bracketType of str) {
+    //loop through the string input
+    if (openBrackets.has(bracketType)) {
+      open.push(bracketType);
+    } else if (
+      bracketCorrespondence[bracketType] &&
+      open.pop() !== bracketCorrespondence[bracketType]
+    ) {
+      //this lines checks whether the brackets match appropriately
+      return "invalid";
+    }
+  }
+
+  if (open.length == 0) {
+    return "valid";
+  } else {
+    return "invalid";
+  }
+}
 
 module.exports = isValid;
