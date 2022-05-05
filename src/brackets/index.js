331c904a584030ea090959b75695013ffe4cc88d
Decagon <decagon@Leslie-Decagons-MacBook-Air.local>
2022-05-05 15:16:23 +0100

    Incomplete due to short deadline

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,61 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+    //Keep track of the length of the string
+    let lenOfStr = str.length;
+    
+    //All strings of brackets must have even length, otherwise invalid
+    if (lenOfStr % 2 !== 0) {
+        return "invalid";
+    }
 
+    //Strings that begin with any of the closing js is invalid
+    if (str[0] === "}" || str[0] === ")" || str[0] === "]") {
+        return "invalid";
+    }
+
+    //Strings that end with an opening j is invalid
+    if (str[lenOfStr - 1] === "{" || str[lenOfStr - 1] === "[" || str[lenOfStr - 1] === "(") {
+        return "invalid";
+    }
+    
+    let countCurl = 0;
+    let countBrac = 0;
+    let countSq = 0;
+
+    for (let r of str) {
+        if (r === "{") {
+            countCurl += 1;
+            continue;
+        }
+        if (r === "}") {
+            countCurl -= 1;
+            continue;
+        }
+        if (r === "(") {
+            countBrac += 1;
+            continue;
+        }
+        if (r === ")") {
+            countBrac -= 1;
+            continue;
+        }
+        if (r === "[") {
+            countSq += 1;
+            continue;
+        }
+        if (r === "]") {
+            countSq -= 1;
+            continue;
+        }
+    }
+    if (countBrac === 0 && countCurl === 0 && countSq === 0) {
+        return "valid";
+    } else {
+        return "invalid";
+    }
+
+
+}
 module.exports = isValid;
--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -4,6 +4,31 @@
  * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
  * @returns {number} The decimal equivalent.
  */
-function romanToDecimal(roman) {}
+function romanToDecimal(roman) {
+    let Numerals = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1
+    };
+
+    var decimal = 0;
+
+    //loop through the string
+    for (let l = 0; l < roman.length; l++) {
+        
+    }
+
+    return decimal;
+}
 
 module.exports = romanToDecimal;
