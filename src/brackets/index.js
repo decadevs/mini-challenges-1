2ca8672fbd328401ecea5b0e946257eb462190f1
Amaechi747 <amaechimoses8@gmail.com>
2022-05-05 15:42:36 +0100

    Solutions 1

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,32 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+    let lengthOfString = str.length
+    // Test only strings with even length
+    if ((lengthOfString % 2) == 0)
+    {
+        for (let i = 0; i <= lengthOfString - 1; i++)
+        {
+            let j = lengthOfString - i;
+            for(j; j >= 1; j - 2)
+            {
+                if (((str[i] == '{' && str[j] == '}') || (str[i] == '{' && str[i + 1] == '}')) 
+                    || ((str[i] == '[' && str[j] == ']') || (str[i] == '[' && str[i + 1] == ']')) 
+                    || ((str[i] == '(' && str[j] == ')') || (str[i] == '(' && str[i + 1] == ')')))
+                {
+                    return 'valid';
+                }
+                else 
+                {
+                    return 'invalid';
+                }
+            }
+
+        }
+    }
+    return 'invalid'; 
+
+}
 
 module.exports = isValid;
--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -4,6 +4,54 @@
  * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
  * @returns {number} The decimal equivalent.
  */
-function romanToDecimal(roman) {}
+function romanToDecimal(roman) 
+{
+    const numberKeys = 
+    {
+        I : parseInt('1'),
+        V : parseInt('5'),
+        X : parseInt('10'),
+        L : parseInt('50'),
+        C : parseInt('100'),
+        D : parseInt('500'),
+        M : parseInt('1000')
+    };
+
+    let total = 0;
+    let temp;
+
+    for(let i = 0; i < roman.length; i++)
+    {
+        let currentNumberIndex = roman.indexOf(roman[i]);
+        let succeedingNumberIndex = roman.indexOf(roman[i + 1]);
+        total += roman[i];
+        for (const numberKey in numberKeys)
+        {
+            if(roman[currentNumberIndex] == numberKey)
+            {
+                for (const secondNumberKey in numberKeys)
+                {
+                    if ((roman[succeedingNumberIndex] == secondNumberKey) && (numberKeys[secondNumberKey] > numberKeys[numberKey]))
+                    {
+                        total = total - numberKeys[numberKey];
+                        total = total + (numberKeys[secondNumberKey] - numberKeys[numberKey]);
+                        i = succeedingNumberIndex;
+                        continue;
+                    } 
+
+                }
+            }
+                // total += roman[i];
+        }
+      
+    }
+
+    // let arr = roman.split('');
+    // for (let i = 0; i < arr.length; i++)
+    // {
+
+    // }
+
+}
 
 module.exports = romanToDecimal;
