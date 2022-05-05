453d1d4c0585629bbf374465681f9f5f7b161fe5
Ugwuanyi Love Ekene <49119675+Hubnotch@users.noreply.github.com>
2022-05-05 19:09:28 +0100

    commit of the algorithm assignment

--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,18 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+    let row = array[0].length
+    for (let j = 0; j < row; j++) {
+        let temp = []
+        for (let i = 0; i < array.length; i++) {
+            temp.push([i][j])
+
+        }
+        output.push(temp)
+    }
+    return output;
+}
 
 module.exports = transpose;
+
