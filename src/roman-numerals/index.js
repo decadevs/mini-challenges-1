045a3fac15e5268ae2eba633df61047732c7c038
Amaechi747 <amaechimoses8@gmail.com>
2022-05-05 16:43:37 +0100

    Second Commit

--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -17,41 +17,87 @@ function romanToDecimal(roman)
         M : parseInt('1000')
     };
 
+
+
+    let arr = roman.split('');
     let total = 0;
-    let temp;
-
-    for(let i = 0; i < roman.length; i++)
+    for (let i = 0; i < arr.length; i++)
     {
-        let currentNumberIndex = roman.indexOf(roman[i]);
-        let succeedingNumberIndex = roman.indexOf(roman[i + 1]);
-        total += roman[i];
-        for (const numberKey in numberKeys)
+        switch(arr[i])
         {
-            if(roman[currentNumberIndex] == numberKey)
-            {
-                for (const secondNumberKey in numberKeys)
+            case 'M':
+                if(arr[i - 1] == 'C')
                 {
-                    if ((roman[succeedingNumberIndex] == secondNumberKey) && (numberKeys[secondNumberKey] > numberKeys[numberKey]))
-                    {
-                        total = total - numberKeys[numberKey];
-                        total = total + (numberKeys[secondNumberKey] - numberKeys[numberKey]);
-                        i = succeedingNumberIndex;
-                        continue;
-                    } 
-
+                    total = total
                 }
-            }
-                // total += roman[i];
+                else
+                {
+                    total = total + 1000;
+                }
+                
+            case 'D':
+                if(a)
+                total = total + 500;
+            case 'C':
+                if(a)
+                total = total + 100;
+            case 'L':
+                if(a)
+                total = total + 50;
+            case 'X':
+                if(a)
+                total = total + 10;
+            case 'V':
+                if(a)
+                total = total + 5;
+            case 'I':
+                if(a)
+                total = total + 1;
         }
-      
     }
-
-    // let arr = roman.split('');
-    // for (let i = 0; i < arr.length; i++)
-    // {
-
-    // }
 
 }
 
+// const numberKeys = 
+// {
+//     I : parseInt('1'),
+//     V : parseInt('5'),
+//     X : parseInt('10'),
+//     L : parseInt('50'),
+//     C : parseInt('100'),
+//     D : parseInt('500'),
+//     M : parseInt('1000')
+// };
+
 module.exports = romanToDecimal;
+
+
+
+    // let total = 0;
+    // let temp;
+
+    // for(let i = 0; i < roman.length; i++)
+    // {
+    //     let currentNumberIndex = roman.indexOf(roman[i]);
+    //     let succeedingNumberIndex = roman.indexOf(roman[i + 1]);
+    //     total += roman[i];
+    //     for (const numberKey in numberKeys)
+    //     {
+    //         if(roman[currentNumberIndex] == numberKey)
+    //         {
+    //             for (const secondNumberKey in numberKeys)
+    //             {
+    //                 if ((roman[succeedingNumberIndex] == secondNumberKey) && (numberKeys[secondNumberKey] > numberKeys[numberKey]))
+    //                 {
+    //                     total = total - numberKeys[numberKey];
+    //                     total = total + (numberKeys[secondNumberKey] - numberKeys[numberKey]);
+    //                     i = succeedingNumberIndex;
+    //                     continue;
+    //                 } 
+
+    //             }
+    //         }
+    //             // total += roman[i];
+    //     }
+      
+    // }
