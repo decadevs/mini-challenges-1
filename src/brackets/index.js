67425b030c13aa4972aa820b73a69cf796f557d7
Bonaventure Umeokwonna <bonaventure.umeokwonna@decagon.dev>
2022-05-05 16:42:48 +0100

    my solutions

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,62 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+    
+    for(let i=0; i<str.length; i++){
+        let tag = str[i];
+        if(!check_balance_within_bracket(str,"{", i)){
+            return "invalid";
+        }
+        if(!check_balance_within_bracket(str,"[",i)){
+            return "invalid";    
+        }
+        if(!check_balance_within_bracket(str,"(", i)){
+            return "invalid";
+        }
+        
+        
+    }
+    return "valid";
+}
+
+
+
+
+function check_balance_within_bracket(str, bracket_type, str_cursor){
+    let openings = ["{", "[", "("];
+    let closings = ["}", "]", ")"];
+
+    
+    index_of_bracket_type = openings.findIndex(bracket_type);
+    
+    if(index_of_bracket_type != -1){
+
+        let other_opening_brackets = openings.splice(index_of_bracket_type,1);
+        let other_closing_brackets = closings.splice(index_of_bracket_type,1);
+    
+        let bracket_1_count = 0;
+        let bracket_2_count = 0;
+
+        let j = str_cursor;
+        while(str[j] != closings[index_of_bracket_type]){
+            if(str[j] == other_opening_brackets[0] || str[j] == other_closing_brackets[0]){
+                bracket_1_count++;
+            }
+
+            if(str[j] == other_opening_brackets[1] || str[j] == other_closing_brackets[1]){
+                bracket_2_count++;
+            }
+            j++;
+
+        }
+        if((bracket_1_count % 2 != 0) || (bracket_2_count % 2 != 0)){
+            return false;
+        }else{
+            return true;
+        }
+        return false;
+    }
+}
 
 module.exports = isValid;
--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,22 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+    let arr_height = array.length;
+    let arr_width = array[0].length;
+    let Arr_trans = [];
+    //for each column collate upto width;
+    //collate all elements on each row upto the height length;
+    //append to the trans-array
+    for(let i = 0; i < arr_width; i++){
+        let arr = [];
+        //At instance column; collate each row's element 
+        for(let j = 0; j < arr_height; j++){
+            arr.push(array[j][i]); 
+        }
+        Arr_trans.push(arr);
+    }
+    return Arr_trans;
+}
 
 module.exports = transpose;
