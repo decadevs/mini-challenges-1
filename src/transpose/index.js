b8b4d78549fa16fe3a008ceea2bca96b62598e27
Decagon <decagon@Investor15.local>
2022-05-05 16:05:03 +0100

    committed to bond

--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,10 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+    let [rotate] = array;
+    return rotate.map((val, columns) => array.map(rotate => rotate[columns]));
+}
 
 module.exports = transpose;
+
