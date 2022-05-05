7eb3c03bd5477ae5a4736685afe75e2c1fde7708
0xluwamania <odedina.adewunmi@outlook.com>
2022-05-05 15:44:33 +0100

    Odedina Adewunmi commit

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,27 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+    if (str.length % 2 !== 0) {
+        return console.log("invalid");
+        } 
+    if (str.indexOf("}") < str.indexOf("{") || str.indexOf(")") < str.indexOf("(") || str.indexOf("]") < str.indexOf("[") ) {
+        return console.log('invalid')
+        }
+    for (let i = 0; i < str.length; i++){
+    if (str[i]=== '{') {
+    let re = str.match(/\d\{\d/g).length === str.match(/\d\}\d/g).length ? "valid": "invalid";
+    return console.log(re);
+    }
+    if (str[i] === '(') {
+        let re = str.match(/\d\(\d/g).length === str.match(/\d\)\d/g).length ? "valid" : "invalid";
+        return console.log(re); 
+        }
+    if (str[i]=== '[') {
+         let re = str.match(/\d\[\d/g).length === str.match(/\d\]\d/g).length ? "valid": "invalid";
+        return console.log(re);
+        }
+        }
+}
 
 module.exports = isValid;
--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -4,6 +4,46 @@
  * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
  * @returns {number} The decimal equivalent.
  */
-function romanToDecimal(roman) {}
+
+    function romanToDecimal(roman) {
+        let num = 0;
+        for (let i = 0; i < roman.length; i++) {
+          if (roman[i] === "M") {
+            num += 1000;
+          } else if (roman[i] === "C" && roman[i + 1] === "M") {
+            num += 900;
+            i++;
+          } else if (roman[i] === "C" && roman[i + 1] === "D") {
+            num += 400;
+            i++;
+          } else if (roman[i] === "D") {
+            num += 500;
+          } else if (roman[i] === "C") {
+            num += 100;
+          } else if (roman[i] === "X" && roman[i + 1] === "C") {
+            num += 90;
+            i++;
+          } else if (roman[i] === "X" && roman[i + 1] === "L") {
+            num += 40;
+            i++;
+          } else if (roman[i] === "L") {
+            num += 50;
+          } else if (roman[i] === "X") {
+            num += 10;
+          } else if (roman[i] === "I" && roman[i + 1] === "X") {
+            num += 9;
+            i++;
+          } else if (roman[i] === "I" && roman[i + 1] === "V") {
+            num += 4;
+            i++;
+          } else if (roman[i] === "V") {
+            num += 5;
+          } else if (roman[i] === "I") {
+            num += 1;
+          }
+        }
+        return num;
+      }
+      
 
 module.exports = romanToDecimal;
