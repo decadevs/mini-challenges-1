d348781c5653485e31a66a2e1b75a45d0d392d69
Decagon <decagon@Leslie-Decagons-MacBook-Air.local>
2022-05-06 23:39:46 +0100

    Solved the brackets algorithm

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -13,52 +13,72 @@ function isValid(str) {
         return "invalid";
     }
 
-    //Strings that begin with any of the closing js is invalid
+    //Strings that begin with any of the closing braces is invalid
     if (str[0] === "}" || str[0] === ")" || str[0] === "]") {
         return "invalid";
     }
 
-    //Strings that end with an opening j is invalid
+    //Strings that end with an opening brace is invalid
     if (str[lenOfStr - 1] === "{" || str[lenOfStr - 1] === "[" || str[lenOfStr - 1] === "(") {
         return "invalid";
     }
     
-    let countCurl = 0;
-    let countBrac = 0;
-    let countSq = 0;
 
-    for (let r of str) {
-        if (r === "{") {
-            countCurl += 1;
-            continue;
+    for (let i in str) {
+        if (str[i] === `{`) {
+            
+            if (foundClose(str, `}`, i)) {
+                continue;
+            } else {
+                return `invalid`;
+            }
         }
-        if (r === "}") {
-            countCurl -= 1;
-            continue;
+
+        if (str[i] === `(`) {
+            
+            if (foundClose(str, `)`, i)) {
+                continue;
+            } else {
+                return `invalid`;
+            }
         }
-        if (r === "(") {
-            countBrac += 1;
-            continue;
-        }
-        if (r === ")") {
-            countBrac -= 1;
-            continue;
-        }
-        if (r === "[") {
-            countSq += 1;
-            continue;
-        }
-        if (r === "]") {
-            countSq -= 1;
-            continue;
+
+        if (str[i] === `[`) {
+            
+            if (foundClose(str, `]`, i)) {
+                continue;
+            } else {
+                return `invalid`;
+            }
         }
     }
-    if (countBrac === 0 && countCurl === 0 && countSq === 0) {
-        return "valid";
-    } else {
-        return "invalid";
-    }
-
-
+    return `valid`;
 }
+
+
+function foundClose(arr, brac, ind) {
+    let brace = arr.indexOf(brac, ind);
+    
+    do {
+        if(brace - ind === 1) {
+            return true;
+        }
+        if(arr[brace - 1] === arr[brace - 2] && arr[brace - 2] === arr[ind]) {
+            brace = arr.indexOf(brac, brace + 1);
+        } else if ((brace - ind) % 2 === 1) {
+            if (arr[brace - 1] === `{` || arr[brace - 1] === `(` || arr[brace - 1] === `[`) {
+               if (arr[brace - 2] === `{` || arr[brace - 2] === `[` || arr[brace - 2] === `(`) {
+                    return false;
+               }
+            }
+            return true;
+        } else {
+            brace = arr.indexOf(brac, brace + 1);
+        }
+    } while (brace > -1);
+    if (brace < 0) {
+        return false;
+    }
+}
+console.log(isValid("((())(())(())(()))"));
 module.exports = isValid;
