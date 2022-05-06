ea754e309847c1d71340445e36fbfd131c983dde
Raymond Umukoro <reiymondjon134@gmail.com>
2022-05-06 20:42:36 +0100

    Worked on transpose

--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,21 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+    let sub_Array = [];
+    let new_Array =[];
+for (let i = 0; i < array[0].length; i++){
+
+
+    for(let j = 0; j < array.length; j++){
+        sub_Array.push(array[j][i]);
+
+    }
+    new_Array.push(sub_Array);
+}
+
+    return(new_Array);
+}
+
 
 module.exports = transpose;
