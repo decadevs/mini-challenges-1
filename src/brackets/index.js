85ca51783af7b3f9d1481a47f52e65113fafdbdc
Darth Vve <oshevire.majoroh@decagon.dev>
2022-06-17 15:31:28 +0100

    Version 1

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,22 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(param) {
+  let check = [];
+  let bracket = { ')': '(', '}': '{', ']': '['};
+  for (let i of param) {
+    if (!bracket[i]){
+       check.push(i);
+    } else if (check.pop() !== bracket[i]) {
+        return "invalid";
+    }
+  }
+  if (check.length === 0) {
+    return "valid";
+  }
+}
 
+/*console.log(isValid(`{[()[]]()}()`))
+console.log(isValid(`[(])`))
+console.log(isValid("}{{{{}}}}{([])(((()))){"))*/
 module.exports = isValid;
