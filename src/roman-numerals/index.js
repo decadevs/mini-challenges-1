1bebbec084b36d9adf0d2f15d8733400283f8ffe
nurez305 <saliu.nurudeen@yahoo.com>
2022-06-18 11:49:22 +0100

    Solution

--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -5,22 +5,22 @@
  * @returns {number} The decimal equivalent.
  */
 function romanToDecimal(roman) {
-    const converter = {
-        M: 1000,
-        CM: 900,
-        D: 500,
-        CD: 400,
-        C: 100,
-        XC: 90,
-        L: 50,
-        XL: 40,
-        X: 10,
-        IX: 9,
-        V: 5,
-        IV: 4,
-        I: 1,
-    }
-    let number = ''
+    // const converter = {
+    //     M: 1000,
+    //     CM: 900,
+    //     D: 500,
+    //     CD: 400,
+    //     C: 100,
+    //     XC: 90,
+    //     L: 50,
+    //     XL: 40,
+    //     X: 10,
+    //     IX: 9,
+    //     V: 5,
+    //     IV: 4,
+    //     I: 1,
+    // }
+    // let number = ''
 
     for (let key in converter){
         //console.log(key)
