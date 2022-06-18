b48b8b3cfd82aad03c0a2db852905c36dfe5a81e
nurez305 <saliu.nurudeen@yahoo.com>
2022-06-18 11:45:15 +0100

    Solution  to week1 assignment

--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -4,6 +4,36 @@
  * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
  * @returns {number} The decimal equivalent.
  */
-function romanToDecimal(roman) {}
+function romanToDecimal(roman) {
+    const converter = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1,
+    }
+    let number = ''
+
+    for (let key in converter){
+        //console.log(key)
+        //console.log(converter[key])
+        while(roman >= converter[key]){
+            number += key;
+            roman -= converter[key];
+        }
+    }
+    return number
+
+    
+}
+console.log(romanToDecimal(14))
 
 module.exports = romanToDecimal;
--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,27 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+    const result = [];
+    for (let row = 0; row < array.length; row++){
+       // console.log(row)
+        for(let column = 0; column < array[row].length; column++){
+            //console.log(column)
+            if(!result[column]){
+                result.push([column])
+            
+            }
+                result[column][row] = array[row][column]
+           
+        }
+    }
+    return result
+}
+
+console.log(transpose([
+    [5, 12, 17, 9, 3],
+    [13, 4, 8, 14, 1],
+    [9, 6, 3, 7, 21],
+  ]))
 
 module.exports = transpose;
