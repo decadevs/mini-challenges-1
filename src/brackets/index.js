b0e32308f229c7e6c154c6274321ef4ea941ce4f
Dec <dec@Excel.local>
2022-06-20 00:55:23 +0100

    challenge-1 Solution

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,43 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+  // declaring the symbols in Key value pairs ( opening and closing brackets)
+
+  const hashMap = { "{": "}", "(": ")", "[": "]" };
+// '(){}[]'
+  // declaring an empty array to push characters into
+
+  const brackets = [];
+
+  // iterating through each character of "str" the input value
+
+  for (let character of str) {
+    // passing a conditional statment, if a character is included in the declared hashMap
+
+    if (hashMap[character]) {
+      // It should be pushed into the newly created array "Brackets"
+
+      brackets.push(hashMap[character]);
+    }
+
+    // If the length of the new array is greater than 0 and the last element in the new array is equal to any character then remove such element "pop()"
+    else if (
+      (brackets.length > 0 && brackets[brackets.length - 1] === character)
+    ) {
+      brackets.pop();
+    }
+    // else it should return a false "Invalid"
+    else {
+      return `invalid`;
+    }
+  }
+  // Coming outside the loop if it meets the first condition return a "valid"
+  if (brackets.length === 0) {
+    return `valid`
+  } else {
+    return `invalid`
+  }
+}
 
 module.exports = isValid;
--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -4,6 +4,37 @@
  * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
  * @returns {number} The decimal equivalent.
  */
-function romanToDecimal(roman) {}
+function romanToDecimal(roman) {
+  let total = 0;
+  let input = roman.toLowerCase();
+   let i = 0;
+
+  const converter = {
+    i: 1,
+    v: 5,
+    x: 10,
+    l: 50,
+    c: 100,
+    d: 500,
+    m: 1000,
+  }
+
+  while (i < input.length) {
+    let current = input[i];
+    console.log(current)
+    let next = input[i + 1];
+    console.log(next)
+
+    if (converter[current] < converter[next]) {
+      total += converter[next] - converter[current]
+      i += 2
+    }
+     else {
+    total += converter[current]
+      i++
+  }
+}
+  return total;
+}
 
 module.exports = romanToDecimal;
