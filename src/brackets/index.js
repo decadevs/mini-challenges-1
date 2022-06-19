ef8dce005b5a58586416eacbb156cb742ec31ad6
Dec <dec@Oluwaseun.local>
2022-06-19 23:24:02 +0100

    AlgoSolution-1

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,38 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+
+    // Create a string template of a valid brackets
+    let validBrackets = '[]{}()';
+
+    // create a stack array to hold stack occurences
+    let stackOccurence = [];
+
+    // A for...of loop is used to access string element (rather than indexs for regular for loops)
+    for (let elem of str){
+        // Get the index at which input bracket occur in validBrackets
+        let bracketIndex = validBrackets.indexOf(elem)
+
+        // from validBrackets, we Openings will have even indexes [0, 2, 4] and
+        // Closings will have odd indexes [1, 3, 5]
+
+        if(bracketIndex % 2 === 0){
+            stackOccurence.push(bracketIndex + 1) // Push the opposite of whats found
+        }else{
+            // Pop out the element and check if it matches current bracketIndex
+            let oddElem = stackOccurence.pop()
+            if(oddElem !== bracketIndex){
+                return 'invalid'
+            }
+        }
+    }
+    // if valid, stackOccurence array must have popped out all matching brackets
+    if(stackOccurence.length === 0){
+        return 'valid'
+    }else{
+        return 'invalid'
+    }
+}
 
 module.exports = isValid;
--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,31 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+    
+    // This defines that code should fail if column or row element is more than 10
+    if (array[0].length > 10 || array.length > 10){ 
+        return 'Not within scope'
+    }
 
+    let columnLength = array[0].length; // Number of element in first sub array
+
+    rowArray = []; // Define an empty array to hold new row
+
+    for(let i = 0; i < columnLength; i++){ // Loop over the column elements (Inner Elements)
+
+        let columnArray = []; // Define empty array to hold new column
+
+        for(let j = 0; j < array.length; j++){ // loop over the row elements
+            // copy rows into columns 
+            columnArray[j] = array[j][i];            
+        }
+        // Push the new column into the row Array
+        rowArray.push(columnArray)
+        
+    }
+    return rowArray;
+}
+    
 module.exports = transpose;
+
