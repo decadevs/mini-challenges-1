578198be49078cfd51478652d5375a49e137363d
Imeh Usoro <imeusoro@rocketmail.com>
2022-06-19 08:38:39 +0100

    brackets done

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,23 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+  let stack = [];
+  let brackets = {
+    "{": "}",
+    "[": "]",
+    "(": ")",
+  };
+  for (let i = 0; i < str.length; i++) {
+    if (brackets[str[i]]) {
+      stack.push(str[i]);
+    } else if (brackets[stack[stack.length - 1]] === str[i]) {
+      stack.pop();
+    } else {
+      return "invalid";
+    }
+  }
+  return stack.length === 0 ? "valid" : "invalid";
+}
 
 module.exports = isValid;
