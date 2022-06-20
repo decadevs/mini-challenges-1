48f63ced819fb1574dfda40b072e94f944bb7162
Vincent Agbapulonwu <vincentmicheal56@gmail.com>
2022-06-20 13:28:05 +0100

    Mini-Challenges1

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -5,7 +5,39 @@
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
 function isValid(str) {
-    console.log(str)
+  splitStr = str.split('');
+  let resultArr = [];
+  let validStr = "valid";
+
+  for(let i=0; i < splitStr.length; i++){
+    let bracket = splitStr[i];
+
+    switch(bracket){
+      case '(':
+      case '{':
+      case '[':
+        resultArr.push(bracket);
+        break;
+      case ')':
+      case '}':
+      case ']':
+        if(resultArr.length == 0){
+            validStr= "invalid";
+        }
+        else{
+            let poppedBracket = resultArr.pop();
+            if((bracket ==')' && poppedBracket !== '(') ||
+                (bracket =='}' && poppedBracket !== '{') ||
+                (bracket ==']' && poppedBracket !== '[')
+            ){
+                validStr = "invalid"
+            }
+        }
+        break;
+      default:
+    }
+  }
+    return resultArr.length > 0 ? validStr = "invalid" : validStr;
 }
 
 module.exports = isValid;
--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,20 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+
+ function transpose(array) {
+  const rows = array.length;
+  const column = array[0].length;
+  const newArr = [];
+  for (let j = 0; j < column; j++) {
+    newArr[j] = Array(rows);
+  }
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < column; j++) {
+      newArr[j][i] = array[i][j];
+    }
+  }
+  return newArr;
+  }
 
 module.exports = transpose;
