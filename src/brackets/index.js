be74ea701bc8caa4f0f5c57f6fe3468291c60d54
RagnarAdaidu <adaidu3@gmail.com>
2022-06-20 20:24:56 +0100

    my solution

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,50 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+  let stack = [];
+
+
+  for(let i = 0; i < str.length; i++)
+  {
+      let x = str[i];
+
+      if (x == '(' || x == '[' || x == '{')
+      {
+
+          // Push the element in the stack
+          stack.push(x);
+
+      }
+
+      if (stack.length == 0)
+          return "invalid";
+
+      let check;
+      if (x == ')') {
+        check = stack.pop();
+        if (check == '{' || check == '['){
+          return 'invalid';
+        }
+      } else if (x == '}'){
+        check = stack.pop();
+        if (check == '(' || check == '[') {
+          return 'invalid';
+        }
+      } else if (x == ']') {
+        check = stack.pop();
+        if (check == '(' || check == '{'){
+          return 'invalid';
+        }
+      }
+  }
+
+  // Check Empty Stack
+  if (stack.length == 0) {
+    return 'valid';
+  } else {
+    return 'invalid';
+  }
+}
 
 module.exports = isValid;
