ad10107662a1b06fa3d39ac535da31156d157d3c
jamesobed <jamesobed4@gmail.com>
2022-06-20 13:33:14 +0100

    mini challenge 1 solution

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,39 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
-
+function isValid(str) {
+    
+        // define stack as openingBracketArray
+        let openingBracketArray = [];
+      
+        if (str.length % 2 != 0) {
+          return "invalid";
+        }
+        for (let i of str) {
+          if (i === "(" || i === "[" || i === "{") {
+            openingBracketArray.push(i);
+          } else if (
+            openingBracketArray.length != 0 &&
+            i === ")" &&
+            openingBracketArray[openingBracketArray.length - 1] === "("
+          ) {
+            openingBracketArray.pop();
+          } else if (
+            openingBracketArray.length != 0 &&
+            i === "]" &&
+            openingBracketArray[openingBracketArray.length - 1] === "["
+          ) {
+            openingBracketArray.pop();
+          } else if (
+            openingBracketArray.length != 0 &&
+            i === "}" &&
+            openingBracketArray[openingBracketArray.length - 1] === "{"
+          ) {
+            openingBracketArray.pop();
+          } else {
+            return "invalid";
+          }
+        }
+        return openingBracketArray.length ? "invalid" : "valid"; 
+}
 module.exports = isValid;
--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,16 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+    let arr = [];
+
+    for (let i = 0; i < array.length; i++) {
+      for (let j = 0; j < array[i].length; j++) {
+        if (!arr[j]) arr[j] = [];
+        arr[j][i] = array[i][j];
+      }
+    }
+    return arr;
+}
 
 module.exports = transpose;
