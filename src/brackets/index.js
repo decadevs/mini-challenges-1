3324f98cc73d7e78d16a26866850b963ecc18101
Basil Ohuruogu <ohuruogubasil@gmail.com>
2022-06-20 15:19:06 +0100

    initial commit

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,35 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+    let arrbracket = [];
+    let openPair = {
+        '{': '}',
+        '[': ']',
+        '(': ')'
+    };
+    let closePair = {
+        '}': true,
+        ']': true,
+        ')': true
+    }
+
+    for (let i = 0; i < str.length; i++){
+        let pairs = str[i];
+
+        if (openPair[pairs]) {
+            arrbracket.push(pairs)
+        } else if (closePair[pairs]) {
+            if(openPair[arrbracket.pop()] !== pairs) return false
+        }
+    }
+
+    // if (str === '' || str == ' ') return 'No bracket pair'
+    return arrbracket.length ? 'invalid' : 'valid'
+}
+
+
+
+
 
 module.exports = isValid;
