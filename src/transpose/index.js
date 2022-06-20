938549e90eb33377dadceb8f3ed6c63586001009
Demilade Otunaiya <otunaiyademilade@gmail.com>
2022-06-20 12:29:18 +0100

    Task 1 Algorithm Solutions

--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,17 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+    let transposedArr=[]
+        for(let i=0; i< array.length; i++){
+          for(let j=0; j<array[i].length; j++){
+           if(!transposedArr[j]) transposedArr[j] = [];
+           transposedArr[j][i] = array[i][j]
+          }
+        };
+      
+            
+        return transposedArr;
+}
 
 module.exports = transpose;
