0b35e5c4937c1f70da6fc7b5acec094dcdd3c11e
RagnarAdaidu <adaidu3@gmail.com>
2022-06-20 17:08:44 +0100

    mini-challenges-1 solution

--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -5,25 +5,27 @@
  * @returns {number} The decimal equivalent.
  */
 function romanToDecimal(roman) {
-  var data = {M: 1000, D: 500, C: 100, L: 50, X: 10, V: 5, I: 1};
-  var numbers = roman.split('');
-  var sum = 0, i;
-
-  for(i = 0; i < numbers.length; i++)
-  {
-    console.log(data[numbers[i]])
-    if(data[numbers[i]] < data[numbers[i+1]])
-    {
-      sum += data[numbers[i+1]] - data[numbers[i]];
-      i++;
-    }
-    else
-    {
-      sum += data[numbers[i]];
-    }
+  let data = {
+    M : 1000,
+    D : 500,
+    C : 100,
+    L : 50,
+    X : 10,
+    V : 5,
+    I : 1
   }
 
-  return sum;
+  let total = 0;
+
+  for (let i = 0; i < roman.length; i++) {
+    if (data[roman[i]] < data[roman[i + 1]]) {
+      total += roman[i + 1] - roman[i];
+      i++;
+    } else {
+      total += roman[i];
+    }
+  }
+  return total;
 }
 
 module.exports = romanToDecimal;
