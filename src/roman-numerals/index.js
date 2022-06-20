526015b813854102c6a81a909396aa097aa1616c
UjSilvia <obianuju1994@gmail.com>
2022-06-20 16:02:32 +0100

    mini-challenge-1

--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -4,6 +4,34 @@
  * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
  * @returns {number} The decimal equivalent.
  */
-function romanToDecimal(roman) {}
+function romanToDecimal(roman) {
+  if (roman == null) return -1;
+  var num = charToInt(roman.charAt(0));
+  var pre, curr;
+
+   for (var i = 1; i <roman.length; i++){
+     curr = charToInt(roman.charAt(i));
+     pre = charToInt(roman.charAt(i-1));
+   if(curr <= pre){
+     num += curr;
+  }else {
+     num = num - pre*2 + curr;
+  }
+  }
+  return num;
+}
+
+function charToInt(c){
+  switch(c){
+    case 'I': return 1;
+    case 'V': return 5;
+    case 'X': return 10;
+    case 'L': return 50;
+    case 'C': return 100;
+    case 'D': return 500;
+    case 'M': return 1000;
+    default : return -1;
+  }
+}
 
 module.exports = romanToDecimal;
--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,25 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+  const result = []
+
+ for (let row = 0; row < arr.length; row++)  {
+ for (let column = 0; column < arr[row].length; column++){
+
+   if(!result[column]) result[column] = []
+   result[column][row] = arr[row][column]
+
+
+}
+}
+
+return result
+}
+let valueIndex = [
+  [5, 12, 17, 9, 3],
+  [13, 4, 8, 14, 1],
+  [9, 6, 3, 7, 21],];
+
 
 module.exports = transpose;
