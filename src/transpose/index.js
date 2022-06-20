253775fbf6dd55aee5a53b071f2ff46139f736f0
Dec <dec@Richie.local>
2022-06-20 12:34:35 +0100

    Solution Challenge 1

--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,22 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+let answer =[];
+ for(let i=0; i<array[0].length; i++){
+    answer.push([]);
+
+    for(let j=0; j<array.length; j++){
+      answer[i].push(array[j][i])
+    }
+
+ }
+
+
+
+ return answer;
+
+
+}
 
 module.exports = transpose;
