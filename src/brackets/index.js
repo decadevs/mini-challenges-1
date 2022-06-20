f0ed09ac152f3113e2a9426722bfac0743b7d3a7
Mac <mac@Michelle.local>
2022-06-20 12:46:45 -0700

    mini-challenge-1 solution

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,40 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+  let bracketsArray = [];
+  let elem;
+
+  for (let i = 0; i < str.length; i++) {
+    elem = str[i];
+      if (elem === '{' || elem === '(' || elem === '[') {
+        bracketsArray.push(elem);
+      }
+
+      let poppedBracket;
+      switch (elem) {
+        case '}' :
+          poppedBracket = bracketsArray.pop();
+          if (poppedBracket !== '{')
+            return 'invalid'
+          break;
+        case ']' :
+          poppedBracket = bracketsArray.pop();
+          if (poppedBracket !== '[')
+            return 'invalid';
+          break;
+        case ')' :
+          poppedBracket = bracketsArray.pop();
+          if (poppedBracket !== '(')
+            return 'invalid'
+          break;
+      }
+    }
+    if (bracketsArray.length === 0) {
+      return 'valid';
+    } else {
+      return 'invalid';
+    }
+}
 
 module.exports = isValid;
--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,16 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+  let outcome = [];
+
+  for (let row = 0; row < array.length; row++) {
+    for (let column = 0; column < array[row].length; column++) {
+      if (!outcome[column]) outcome[column] = [];
+      outcome[column][row] = array[row][column];
+    }
+  }
+  return outcome;
+}
 
 module.exports = transpose;
