d47d7e5f21d03dd2c749c695ba8952e3b76c116b
Decagon <decagon@Decagons-MacBook-Air.local>
2022-06-20 10:21:53 +0100

    ushashir solutions

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,29 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+    // declare open / close tags strings
+    let items = '{}[]()';
+
+    // declare empty stack
+    let stack = [];
+
+    // loop through str to ensure each element contains both openining and close branket
+    for(let item of str){
+      let itemsIndex = items.indexOf(item);
+      if(itemsIndex % 2 === 0) {
+        stack.push(itemsIndex + 1)
+      } else {
+        if(stack.pop() !== itemsIndex){
+          return 'invalid';
+        }
+      }
+    }
+    if(stack.length === 0){
+      return 'valid';
+    } else {
+      return 'invalid';
+    }
+}
 
 module.exports = isValid;
--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -4,6 +4,51 @@
  * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
  * @returns {number} The decimal equivalent.
  */
-function romanToDecimal(roman) {}
+
+function romanToDecimal(roman) {
+
+  // function value stores 'IVXLCDM' strings and their decimal equivalent
+  function romanStrings(romanString) {
+    if (romanString == 'I')
+        return 1;
+    if (romanString == 'V')
+        return 5;
+    if (romanString == 'X')
+        return 10;
+    if (romanString == 'L')
+        return 50;
+    if (romanString == 'C')
+        return 100;
+    if (romanString == 'D')
+        return 500;
+    if (romanString == 'M')
+        return 1000;
+    return -1;
+  }
+
+  let result = 0;
+        for (i = 0; i < roman.length; i++){
+            // Get value of roman string and store in s1
+            let s1 = romanStrings(roman.charAt(i));
+
+            // Get value of roman string + 1 and store in s2
+            if (i + 1 < roman.length) {
+                let s2 = romanStrings(roman.charAt(i + 1));
+
+                // Use if statement to compute roman strings principles of addition and substraction
+                if (s1 >= s2) {
+                    result = result + s1;
+                } else {
+                    result = result + s2 - s1;
+                    i++;
+                }
+            } else {
+                result = result + s1;
+            }
+        }
+
+        return result;
+    }
+
 
 module.exports = romanToDecimal;
