556b51a91bf9e2528d0d4f7e06e6a21c9d83873c
beastlywallace <akonakpevwe@gmail.com>
2022-06-20 13:42:55 +0100

    Mini-Chhallenges1

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,45 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+   const bracketArr= [];
+    splittedStr= str.split('');
+
+
+    for(let i=0; i<splittedStr.length; i++){
+      let splittedStrCharacter = splittedStr[i];
+      switch(splittedStrCharacter){
+        case '(':
+        case '{':
+        case '[':
+          bracketArr.push(splittedStrCharacter);
+          break;
+        case ')':
+        case '}':
+        case ']':
+          if(bracketArr.length === 0){
+              return "invalid";
+          }else{
+              let poppedBracket= bracketArr.pop();
+              if((splittedStrCharacter ==')' && poppedBracket != '(') ||
+                  (splittedStrCharacter =='}' && poppedBracket != '{') ||
+                  (splittedStrCharacter ==']' && poppedBracket != '[')
+              ){
+                  return "invalid"
+              }
+          }
+          break;
+        default:
+
+      }
+    }
+
+      if(bracketArr.length > 0){
+          return "invalid";
+      }
+
+      return 'valid';
+
+}
 
 module.exports = isValid;
--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -4,6 +4,51 @@
  * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
  * @returns {number} The decimal equivalent.
  */
-function romanToDecimal(roman) {}
+
+ function romanToDecimal(roman) {
+  let decimalValue =  0;
+
+  for(let i = 0; i< roman.length; i++){
+      let char = roman[i];
+
+      let firstValue = converted(char);
+
+      if(i+1 < roman.length){
+          let secondvalue = converted(roman[i + 1]);
+
+          if(firstValue>= secondvalue){
+              decimalValue = decimalValue + firstValue;
+          }else{
+              decimalValue = decimalValue + secondvalue - firstValue;
+              i++;
+          }
+
+      }else{
+          decimalValue = decimalValue + firstValue;
+      }
+  }
+
+  return decimalValue;
+}
+var container = {
+I: 1,
+V: 5,
+X: 10,
+L: 50,
+C: 100,
+D: 500,
+M: 1000,
+}
+function converted(value){
+  let finaldecimal = 0;
+  for(let j in container){
+      if(value==j){
+          finaldecimal = container[j]
+      }
+   }
+   return finaldecimal;
+}
+
+
 
 module.exports = romanToDecimal;
