6fa5b4471df51d3234080ed4ec07e1ecf5a46e9c
Anuoluwapo <oderinloanuoluwapo@gmail.com>
2022-06-20 13:12:33 +0100

    Anuoluwapo Oderinlo completed mini challenge 1

--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -3,7 +3,87 @@
  *
  * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
  * @returns {number} The decimal equivalent.
- */
-function romanToDecimal(roman) {}
+*/
+function romanToDecimal(roman) {
+    const romanObj = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
 
+
+    let decimal = 0;
+    // let decimal = [];
+
+    /*for (let i = 0; i < roman.length; i++) {
+        // if (i < roman.length) {
+        if (romanObj[roman[i]] < romanObj[roman[i + 1]]) {
+
+            decimal += romanObj[roman[i + 1]] - romanObj[roman[i]]
+            i += 1;
+            console.log(decimal)
+        } else {
+            decimal += romanObj[roman[i]]
+            console.log(decimal)
+        }
+        // }
+
+
+    }*/
+
+    // console.log(decimal)
+
+
+
+
+
+    for (let i = 0; i < roman.length; i++) {
+        // console.log(decimal)
+        if (roman[i] == 'I' && roman[i + 1] == 'V') {
+            decimal += 4;
+            i += 1
+        }
+        else if (roman[i] == 'I' && roman[i + 1] == 'X') {
+            decimal += 9;
+            i += 1
+        }
+        else if (roman[i] == 'X' && roman[i + 1] == 'L') {
+            decimal += 40;
+            i += 1
+        }
+        else if (roman[i] == 'X' && roman[i + 1] == 'C') {
+            decimal += 90;
+            i += 1
+
+        }
+        else if (roman[i] == 'C' && roman[i + 1] == 'D') {
+            decimal += 400;
+            i += 1
+
+        }
+        else if (roman[i] == 'C' && roman[i + 1] == 'M') {
+            decimal += 900;
+            i += 1
+
+        } else {
+            decimal += romanObj[roman[i]];
+        }
+        // console.log(decimal)
+
+    }
+
+    // console.log(decimal);
+    return decimal;
+
+
+
+}
+
+
+// romanToDecimal('XLVII')
 module.exports = romanToDecimal;
+
--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,20 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+	const arr = [];
+
+	for (let i = 0; i < array[0].length; i++) {
+		arr.push([])
+		for (let j = 0; j < array.length; j++) {
+			arr[i].push(array[j][i])
+		}
+	}
+
+	// console.log(arr);
+	return arr
+
+}
+
 
 module.exports = transpose;
