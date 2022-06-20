58f2a695dfb4935abfec0ab693e6e0fa3d92ee49
User <user@Users-MacBook-Air.local>
2022-06-20 13:01:52 +0100

    Challenge1 solution to algorithm

--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -4,6 +4,31 @@
  * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
  * @returns {number} The decimal equivalent.
  */
-function romanToDecimal(roman) {}
+function romanToDecimal(roman) {
+  const ROM = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
 
+}
+let sum = 0;
+let initialPoint, nextPoint;
+for(let i = 0; i < roman.length; i++){
+    initialPoint = roman[i];
+    nextPoint = roman[i + 1];
+    if(ROM[initialPoint] < ROM[nextPoint]){
+        sum -= ROM[initialPoint];
+    }
+    else{
+        sum += ROM[initialPoint];
+    }
+}
+return sum;
+
+}
+console.log(romanToDecimal("CM"))
 module.exports = romanToDecimal;
--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,20 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
-
+function transpose(array) {
+  let arr, temp;
+  let myResult =[];
+  let elem = array[0].length;
+  let arrays = array.length;
+  for(let i = 0; i < elem; i++){
+    arr = [];
+    for(let j = 0; j < arrays; j++){
+      temp = array[j][i];
+      arr.push(temp)
+    }
+    myResult.push(arr)
+  }
+  return myResult
+}
+console.log(transpose([[1,2,3,4],[3,4,5,6]]));
 module.exports = transpose;
