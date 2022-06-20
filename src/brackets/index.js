50433ae7095561776466e7fc9bd842f40634f92c
babsddev <ajayi.gbemi0806@gmail.com>
2022-06-20 13:45:19 +0100

    Solution to week 1 algorithim

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,37 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+  const input = str.split("");
+  const openBrackets = ["(", "{", "["];
+  const closeBrackets = [")", "}", "]"];
+
+  const result = [];
+
+  for (let i = 0; i < input.length; i++) {
+        // if input[i] is in openBrackets
+    if (openBrackets.includes(input[i])) {
+      result.push(input[i]);
+      // console.log(openBrackets);
+    } else if (closeBrackets.includes(input[i])) {
+      // console.log(closeBrackets);
+      const position = closeBrackets.indexOf(input[i]);
+      // console.log(position);
+      if (
+        result.length === 0 ||
+        result[result.length - 1] !== openBrackets[position]
+      ) {
+        return "invalid";
+      } else {
+        result.pop();
+      }
+    }
+  }
+  if (result.length === 0) {
+   return "valid";
+  } else {
+    return "invalid";
+  }
+}
 
 module.exports = isValid;
--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,17 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+  let result = [];
+
+  for (let i = 0; i < array[0].length; i++) {
+    result.push([]);
+
+    for (let j = 0; j < array.length; j++) {
+      result[i].push(array[j][i]);
+    }
+  }
+  return result;
+}
 
 module.exports = transpose;
