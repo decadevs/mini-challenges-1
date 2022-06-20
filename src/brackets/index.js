0ae03430c8f4fb37d1d4662a804e479666d5c9ef
tobbyano1234 <adegbesan86@gmail.com>
2022-06-20 15:34:16 +0100

    mini-challenes-1 solution

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,26 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+
+  const newStr = "(){}[]";
+  const arr = [];
+  for (let i of str) {
+    console.log(i);
+    let current = newStr.indexOf(i);
+    if (current % 2 == 0) {
+      arr.push(current + 1);
+    } else {
+      if (arr.pop() !== current) {
+        return "invalid";
+      }
+    }
+  }
+  if (arr.length !== 0) {
+    return "invalid";
+  } else {
+    return "valid";
+  }
+}
 
 module.exports = isValid;
--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -4,6 +4,29 @@
  * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
  * @returns {number} The decimal equivalent.
  */
-function romanToDecimal(roman) {}
-
+function romanToDecimal(roman) {
+  const romanNew = {
+    M: 1000,
+    D: 500,
+    C: 100,
+    L: 50,
+    X: 10,
+    V: 5,
+    I: 1,
+  };
+  let sum = 0;
+  for (let i = 0; i < roman.length; i++) {
+    let first = roman[i];
+    let second = roman[i + 1];
+    // 'IX' = 1 < 10 => 10 - 1
+    if (romanNew[first] < romanNew[second]) {
+      sum += romanNew[second] - romanNew[first];
+      i++;
+    } else {
+      // 'XI' = 10 > 1 => 10 + 1
+      sum += romanNew[first];
+    }
+  }
+  return sum;
+}
 module.exports = romanToDecimal;
