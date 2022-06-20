0bab0929fcad470676c830a5176e581344554d28
Chukwuma Michael Ezeobika <admin@admins-MacBook-Pro.local>
2022-06-20 10:47:54 -0400

    first assignment

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,38 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+    const openBrackets = ['(', '[', '{'];
+    const closeBrackets = [')', ']', '}'];
+    const checker = [];
+
+    const bracketObject = {
+        '(': ')',
+        '[': ']',
+        '{': '}'
+    };
+
+    for (let bracket of str) {
+
+        if (openBrackets.includes(bracket)) {
+            checker.push(bracket);
+        } else if (closeBrackets.includes(bracket)) {
+
+            if (!checker.length) {
+                return 'invalid';
+            } else if (bracketObject[checker.pop()] !== bracket) {
+                return 'invalid';
+            }
+
+        }
+
+    }
+
+    if (checker.length === 0) {
+        return 'valid';
+    } else {
+        return 'invalid';
+    }
+}
 
 module.exports = isValid;
