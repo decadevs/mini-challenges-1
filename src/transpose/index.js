61a50d951aea680e30ccecfa7f1fb5b8e268ff2a
udokachi <106298075+udokachi@users.noreply.github.com>
2022-09-11 16:13:01 +0100

    Chidinma onyeelukwe  work

--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,25 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+     
+    let answer = [];
+    let temporary = [];
+    let x = 0;
+    for (let index = 0; index < array.length; index++){
+        const i = array[index];
+        if(x < i.length){
+            temporary.push(i[x]);
+        }
+        if(index == array.length - 1 && x < i.length){
+            answer.push(temporary);
+            temporary = []
+            x += 1
+            index = -1
+        }
+    }
+    return answer
+
+}
 
 module.exports = transpose;
