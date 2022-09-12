afd08cb582d61253a0720f4a7f367663bbe64292
skippergoroye <oluwatosinadegoroye21@gmail.com>
2022-09-12 15:48:32 +0100

    challenge 1 solution

--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -4,6 +4,48 @@
  * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
  * @returns {number} The decimal equivalent.
  */
-function romanToDecimal(roman) {}
+// function romanToDecimal(roman) {}
+
+// module.exports = romanToDecimal;
+
+var values;
+values = {
+  "I": 1,
+  "V": 5,
+  "X": 10,
+  "L": 50,
+  "C": 100,
+  "D": 500,
+  "M": 1000
+};
+
+function romanToDecimal(roman) {
+  var i, s1, s2, total;
+  total = 0;
+  i = 0;
+
+  while (i < roman.length) {
+    s1 = values[roman[i]];
+
+    if (i + 1 < roman.length) {
+      s2 = values[roman[i + 1]];
+
+      if (s1 >= s2) {
+        total = total + s1;
+        i = i + 1;
+      } else {
+        total = total - s1;
+        i = i + 1;
+      }
+    } else {
+      total = total + s1;
+      i = i + 1;
+    }
+  }
+
+  return total;
+}
 
 module.exports = romanToDecimal;
+
+console.log(romanToDecimal("MCMXCIV"));
