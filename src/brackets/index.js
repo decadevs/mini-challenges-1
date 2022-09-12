5a451a2b1b42f23b4c2f5b868e94311234391ea1
yinotherhino <muhammedbayero@gmail.com>
2022-09-11 22:28:46 -0700

    challenge 1 tests passed

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,33 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+    // check if string includes matching brackets
+    while(str.includes("()") || str.includes("{}") || str.includes("[]")){
+        //keep replacing any instances of matching brackets till there is none
+        str = str.replace("()", "");
+        str = str.replace("[]", "");
+        str = str.replace("{}", "");
+    }
+    //if there is nothing left in the string return valid
+    if (str.length== 0){
+        return "valid";
+    }
+    //if there is something left in the string which must be unmatching brackets return invalid
+    else{
+        return "invalid"
+    }
+
+    //APPROACH 2
+    //const array of open brackets
+    //const array of close brackets with similar index as its open counterpart
+    //while str includes any of the brackets
+    //start from the lastIndexof open  bracket and check if the next bracket is its counterpart
+    // delete the pair if they match and continue the while loop
+    //else return invalid
+
+
+
+}
 
 module.exports = isValid;
--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,22 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+    //newarray= create an array filled with 0's such that no of cols = no of rows of array and no of rows= no of cols of array
+    let arrayLength= array.length;
+    let arrayElemLength= array[0].length
+    let newArray= Array(arrayElemLength).fill("0".repeat(arrayLength));
+    // for (let i=0; i<newArray; i++){
+    //     newArray[i]= newArray[i].split("");
+    // }
+    newArray.forEach((x,i) => newArray[i]= newArray[i].split(""))
+    for (let i=0; i<arrayElemLength; i++){
+        for (let j=0; j<arrayLength; j++){
+            newArray[i][j]=array[j][i];
+        }
+    }
+    console.log(typeof(newArray[0]))
+    return newArray
+}
 
 module.exports = transpose;
