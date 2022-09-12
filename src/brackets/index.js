5a4888fccb0709f292f7a32346b6b3a3f70760bc
Oluwaseyi <oluwaseyi.makinde@decagon.dev>
2022-09-12 08:52:48 -0700

    add-feature-mini-challenge-1

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,19 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+ 
+function isValid(str) {
+    while(str.includes("()")|| str.includes("[]") || str.includes("{}")){
+      str = str.replace("()", "");
+      str = str.replace("[]", "");
+      str = str.replace("{}", "");
+    }
+      if(str.length > 0){
+         return "invalid"
+      }else {
+         return "valid"
+      }
+
+};   
 
 module.exports = isValid;
