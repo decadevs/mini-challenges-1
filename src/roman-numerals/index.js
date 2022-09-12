fa401fe74ba658fc6a7c1a2dd90a69337f5b193a
Decagon <decagon@Decagons-MacBook-Air.local>
2022-09-12 08:11:13 +0100

    <"add feature challenge 1">

--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -4,6 +4,63 @@
  * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
  * @returns {number} The decimal equivalent.
  */
-function romanToDecimal(roman) {}
+
+function conversion(rom) {
+     if (rom == 'I')
+       return 1;
+    if (rom == 'V')
+        return 5;
+    if (rom == 'X')
+        return 10;
+    if (rom == 'L')
+        return 50;
+    if (rom == 'C')
+        return 100;
+    if (rom == 'D')
+        return 500;
+    if (rom == 'M')
+        return 1000;
+    return -1;
+}
+
+function romanToDecimal(roman){
+
+    let result = 0;
+    for (let i = 0; i < roman.length; i++){
+        //get the value for roman symbol [i]
+        let str1 = conversion(roman.charAt(i));
+        //get the value for roman symbol [i]+1
+        if (i + 1 < roman.length){
+            let str2 = conversion(roman.charAt(i+1));
+        //lets compare the values
+            if(str1 >= str2){
+            // if the value of the current symbol is greater than or equal to 
+            //the next symbol.
+            result = result + str1;
+            }else{
+            // the if the value  of the current symbol is less than the next symbol
+            result = result + (str2 - str1);
+                i++;
+                }
+        }
+        else {
+            result = result + str1;
+        }
+
+
+        }
+         return result;
+      
+      
+ 
+    }
+
+console.log((romanToDecimal("CM")));
+
+
+
+
+
+
 
 module.exports = romanToDecimal;
--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,11 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+    
+        return array[0].map((col, c) => array.map((row, r) => array[r][c]));
+
+    
+}
 
 module.exports = transpose;
