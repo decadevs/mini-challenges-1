6012a966b2c9cef35570ce1acbf02a5bb3d0880a
samuelajalode <113217825+samuelajalode@users.noreply.github.com>
2022-09-13 07:00:52 +0100

    Revert "completed-challenge-1"

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,22 +4,6 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {
-        i = 0;
-        for (let i = 0; i < str.length; i++);
-            if (str[i] == '(');
-                count = i++;
-            str[i] == ')';
-                if (count == 0);
-                    return "invalid";
-                    count--
-        end //for
-    
-        if (count == 0)
-            return "invalid";
-            else
-                return "valid";
-    }
-console.log(isValid(`[(])`));
-console.log(isValid(`{[()[]]()}()`))
+function isValid(str) {}
+
 module.exports = isValid;
--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,16 +4,6 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(a) {
-
-
-  return a[0].map(function (_, c) { return a.map(function (r) { return r[c]; }); });
-  // or in more modern dialect
-  // return a[0].map((_, c) => a.map(r => r[c]));
-}
-console.log(transpose([
-  [1, 3],
-  [2, 4],
-]));
+function transpose(array) {}
 
 module.exports = transpose;
