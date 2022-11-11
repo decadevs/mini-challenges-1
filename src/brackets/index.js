4fbb0af11c8a255c4d143e441fd0c459434db887
samuelajalode <113217825+samuelajalode@users.noreply.github.com>
2022-11-11 09:41:00 +0100

    Update index.js

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,36 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+ function isValid(str) {
+    // code me
+    let stack = [];
+    let map = {
+        '(': ')',
+        '[': ']',
+        '{': '}'
+    }
 
-module.exports = isValid;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === '(' || str[i] === '[' || str[i] === '{') {
+            stack.push(str[i]);
+        } else {
+            let last = stack.pop();
+
+            if (str[i] !== map[last]) {
+                return "invalid";
+            }
+        }
+    }
+
+    if (stack.length !== 0) {
+        return "invalid";
+    }
+
+    return "valid";
+ }
+
+ console.log(isValid("()"));
+    console.log(isValid("()[]{}"));
+    console.log(isValid("(]"));
+ module.exports = isValid;
+ 
