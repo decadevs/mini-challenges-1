50878386f1a0e2adbc5332b05df99d04157ee858
samuelajalode <113217825+samuelajalode@users.noreply.github.com>
2022-11-11 09:52:24 +0100

    Update index.js

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -12,7 +12,9 @@
         '[': ']',
         '{': '}'
     }
-
+    
+module.exports = isValid;
+ 
     for (let i = 0; i < str.length; i++) {
         if (str[i] === '(' || str[i] === '[' || str[i] === '{') {
             stack.push(str[i]);
