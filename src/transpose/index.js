e79a56550240da7a671b5125d5b3e7cfc6f503cb
Emmanuel <38895255+Nixhazel@users.noreply.github.com>
2022-11-26 01:28:45 +0100

    Solved the Transpose Problem

--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,19 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+  let newArr = [];
+
+  for (let i = 0; i < array.length; i++) {
+    newArr.push([]);
+  }
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      newArr[j].push(array[i][j]);
+    }
+  }
+
+  return newArr;
+}
 
 module.exports = transpose;
