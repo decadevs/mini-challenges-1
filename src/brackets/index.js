fbc716939f67ecf8a00bf45059b2d350e5befd46
Chidike Henry <dk1online25@gmail.com>
2022-11-27 22:48:16 +0100

    Solutions to mini challenges 1

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -6,48 +6,21 @@
  */
 
 function isValid(str) {
-  const myArray = str.split("");
-  let normalBracketCounter = 0;
-  let sqBracketCounter = 0;
-  let curlyBracketsCounter = 0;
+     const stack = [];
 
-  let arrayLength = myArray.length - 1;
+     for (const i of str) {
+       if (i === "{" || i === "[" || i === "(") {
+         stack.push(i);
+       } else if (i === "}" && stack.pop() !== "{") {
+         return "invalid";
+       } else if (i === "]" && stack.pop() !== "[") {
+         return "invalid";
+       } else if (i === ")" && stack.pop() !== "(") {
+         return "invalid";
+       }
+     }
 
-  if (
-    (myArray[0] === "(" && myArray[arrayLength] === ")") ||
-    (myArray[0] === "{" && myArray[arrayLength] === "}") ||
-    (myArray[0] === "[" && myArray[arrayLength] === "]")
-  ) {
-    // checking and validating the string
-    for (let i = 0; i < myArray.length; i++) {
-      // loop through each character
-      const j = myArray[i]; // assign each character of the array to the variable j
-      if (j === "(") {
-        normalBracketCounter++; //increments the normal bracket
-      } else if (j === ")") {
-        normalBracketCounter--; //decrements the normal brackets
-      } else if (j === "{") {
-        curlyBracketsCounter++; // increments the curly brackets
-      } else if (j === "}") {
-        curlyBracketsCounter--; // decrements the curly brackets
-      } else if (j === "[") {
-        sqBracketCounter++; // increments the square brackets
-      } else if (j === "]") {
-        sqBracketCounter--; // decrements the square brackets
-      }
-    }
-    if (
-      normalBracketCounter === 0 &&
-      sqBracketCounter === 0 &&
-      curlyBracketsCounter === 0
-    ) {
-      console.log("valid"); // show valid
-    } else {
-      console.log("invalid"); // show invalid
-    }
-  } else {
-    console.log("invalid"); // show invalid
-  }
+     return stack.length > 0 ? "invalid" : "valid";
 }
 
 module.exports = isValid;
