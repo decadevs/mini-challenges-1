ab0bc3fd306fa8e75c53017153cedbd9358e161c
Akingbade Motolani <akingbademotolani@yahoo.co.uk>
2022-11-27 22:56:58 +0100

    mini-challenge 1

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,41 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+  if (str.length % 2 !== 0){
+    return "invalid"
+  }
+  let splitStr = str.split("")
+  let Arr = [];
+  for(let i =0; i < splitStr.length; i++){
+    if(splitStr[i] === '(' || splitStr[i] === '{' || splitStr[i] === '['){
+      Arr.push(splitStr[i])
+    }
+    if(splitStr[i] === '}'){
+      let temp = Arr.pop();
+      if(temp !== '{'){
+        return "invalid"
+      }
+    }
+    if(splitStr[i] === ')'){
+            let temp = Arr.pop();
+            if(temp !== '('){
+                return 'invalid'
+            }
+        }
+        if(splitStr[i] === ']'){
+            let temp = Arr.pop();
+            if(temp !== '['){
+                return 'invalid'
+            }
+        }
+  }
+  if (Arr.length > 0){
+    return "invalid"
+  }
+return "valid"
+}
 
+console.log(isValid("{}{{}}{{{}}}{{{{}}}}{}{}{}"))
 module.exports = isValid;
+
--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -4,6 +4,36 @@
  * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
  * @returns {number} The decimal equivalent.
  */
-function romanToDecimal(roman) {}
+function romanToDecimal(roman) {
+  let order = {
+    I : 1,
+    V : 5,
+    X : 10,
+    L : 50,
+    C : 100,
+    D : 500,
+    M : 1000
+  }
+
+  let dec = 0;
+
+  let romanSplit = roman.split("")
+
+  romanSplit.forEach((dig, i) => {
+    if(order[dig] < order[romanSplit[i+1]]){
+           return dec = dec - order[dig]
+    }
+     else dec += order[dig]
+  });
+
+  return dec;
+}
+
+console.log(romanToDecimal("MIV"))
 
 module.exports = romanToDecimal;
+
+
+// DEC = 1000
+// DEC = -1
+// DEC = 5
