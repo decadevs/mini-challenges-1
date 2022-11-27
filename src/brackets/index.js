065ab59695129edc969c447a8a3918b6d9ed1e99
John <Decadev@Ik-MacBook-Air.local>
2022-11-27 22:40:06 +0100

    solution to mini-challenges-1

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,51 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+
+    if(str[0] === '}' || str[0] === ']' || str[0] === ')') return 'invalid'
+
+    if(str.length % 2 != 0) return 'invalid'
+
+    const obj = {
+        '{': 0,
+        '}': 0,
+        '[': 0,
+        ']': 0,
+        '(': 0,
+        ')': 0
+    }
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i]
+        if (obj[char]>= 0) obj[char]++
+       
+    }
+
+    if (obj['{'] != obj['}'] || obj['('] != obj[')'] || obj['['] != obj[']']) return 'invalid'
+
+    for (let i = 0; i < str.length; i++) {
+
+        if (str.includes('()')) {
+            str = str.split('()').join('')
+        }
+        if (str.includes('[]')) {
+            str = str.split('[]').join('')
+        }
+        if (str.includes('{}')) {
+            str = str.split('{}').join('')
+        }
+    }
+
+    if (str == '()' || str == '[]' || str == '{}' || str == "") return 'valid'
+    
+    let checkBox = ['{]', '{)', '[}', '[)', '(]', '(}']
+
+    for (let i = 0; i < str.length; i++) {
+        let check = str[i] + str[str.length-i-1]
+        if (checkBox.includes(check)) return 'invalid'
+    }
+    return 'valid'
+}
+
 
 module.exports = isValid;
--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,18 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+ function transpose(array) {
+    let result = [];
+    for (let j = 0; j < array[0].length; j++) {
+      let subArray = []
+      
+      for (let i = 0; i < array.length; i++) {
+        let val = array[i][j]
+        subArray.push(val)
+      }
+      result.push(subArray)
+    }
+    return result
+  }
 
 module.exports = transpose;
