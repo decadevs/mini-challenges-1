66820a0e6f573bf513b676064ed78faed8d0d212
Esther Ime Edoho <Starexedoho@gmail.com>
2022-11-27 23:05:45 +0100

    Solutions to mini challenge one

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,45 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+    if (str.length % 2 !== 0){
+        return "invalid"
+      }
+      let splitted = str.split("")
+      let finalArr = [];
+      
+      for(let i =0; i < splitted.length; i++){
+        if(splitted[i] === '(' || splitted[i] === '{' || splitted[i] === '['){
+          finalArr.push(splitted[i])
+          console.log(finalArr)
+        }
+        if(splitted[i] === '}'){
+          let test = finalArr.pop();
+          if(test !== '{'){
+            return "invalid"
+          }
+        }
+        if(splitted[i] === ')'){
+                let test = finalArr.pop();
+                if(test !== '('){
+                    return 'invalid'
+                }
+            }
+            if(splitted[i] === ']'){
+                let test = finalArr.pop();
+                if(test !== '['){
+                    // console.log('here')
+                    return 'invalid'
+                }
+            }
+      }
+      if (finalArr.length > 0){
+        return "invalid"
+      } 
+    return "valid"
+}
+console.log(isValid("{[()[]]()}()"))
+
+
 
 module.exports = isValid;
--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,22 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+ 
+let bigArray = [];
+
+for(let i = 0; i < array[0].length; i++){
+        let innerArray =[];
+        for (let j=0;j<array.length;j++){
+            innerArray.push(array[j][i])
+        }
+        bigArray.push(innerArray)
+    }
+    return bigArray
+}
+
+   
+
+
 
 module.exports = transpose;
