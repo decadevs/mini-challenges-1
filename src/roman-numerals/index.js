f59f2e425b252b0c0e512db998891851fa923cbe
Tobi OLADELE <41081223+tobiadedoyin@users.noreply.github.com>
2022-11-28 06:46:53 +0100

    The first algorithm is closing all valid bracket while the second one is converting roman numeral to interger , the last one is performing matrix transpose

--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -4,6 +4,40 @@
  * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
  * @returns {number} The decimal equivalent.
  */
-function romanToDecimal(roman) {}
+function romanToDecimal(roman) {
+  let romanToDecimal = {
+    I : 1,
+    V : 5,
+    X : 10,
+    L : 50,
+    C : 100,
+    D : 500,
+    M : 1000
+  };
+
+  let number = 0;
+
+ for (let i = 0; i < roman.length; i++) {
+  let currentNumber = romanToDecimal[roman.charAt(i)];
+  let nextNumber = romanToDecimal[roman.charAt(i + 1)];
+
+  if(nextNumber){
+    if (currentNumber >= nextNumber) {
+        number += currentNumber;
+ }else {
+    number += (nextNumber - currentNumber);
+  i++;
+}
+
+} else{
+    number += currentNumber;
+  }
+ }
+ console.log(number);
+ return number;
+}
 
 module.exports = romanToDecimal;
+
+romanToDecimal("MCMXCVI");
+
