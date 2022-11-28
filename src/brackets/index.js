b7eff10b09d0fe7dc9926dd7b9321b841004f738
Deca <deca@Decagons-MacBook-Air.local>
2022-11-28 19:25:11 +0100

    solved mini challenge problem 1

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,26 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(s) {
+  const stack = [];
+
+  for (let i = 0; i < s.length; i += 1) {
+    const top = stack[stack.length - 1];
+    if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
+      stack.push(s[i]);
+    } else if (s[i] === ')' && top === '(' && stack.length !== 0) {
+      stack.pop();
+    } else if (s[i] === ']' && top === '[' && stack.length !== 0) {
+      stack.pop();
+    } else if (s[i] === '}' && top === '{' && stack.length !== 0) {
+      stack.pop();
+    } else {
+      return "invalid";
+    }
+  }
+
+  return stack.length === 0;
+}
+ console.log(isValid("{){}[)"))
 
 module.exports = isValid;
