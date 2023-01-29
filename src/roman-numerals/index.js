ef42e0644858c2e634a03a061b23fb6d756592fb
mac <mac@Blossom-MacBook-Air.local>
2023-01-29 19:31:55 +0100

    first commit

--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -4,6 +4,20 @@
  * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
  * @returns {number} The decimal equivalent.
  */
-function romanToDecimal(roman) {}
+function romanToDecimal(roman) {
+    let result = 0;
+    let splitRoman = roman.split('') // [x, i]
+    const romanNum = {I : 1, V : 5, X : 10, L : 50, C : 100, D : 500, M : 1000} ;
+for(let i = 0; i < splitRoman.length; i++){
+    if(romanNum[splitRoman[i]] < romanNum[splitRoman[i + 1]]){ //
+        result -= romanNum[splitRoman[i]]
+    }else {
+        result += romanNum[splitRoman[i]]
+    }
+}
+return result;
+}
 
 module.exports = romanToDecimal;
+
+
--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,20 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+    if(array.length === 0) {
+        return [];
+    }
+    let rows = array[0].length;
+    let columns = array.length;
+    let result = [];
+for(let i = 0; i < rows; i++){
+    result[i] = [];
+    for(let j = 0; j < columns; j++){
+        result[i][j] = array[j][i]
+    }
+}
+return result;
+}
 
 module.exports = transpose;
