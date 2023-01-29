bf109f1a0ec3efb280867232ec67543653ba663f
user <user@users-MacBook-Air.local>
2023-01-29 19:21:51 +0100

    master

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,23 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+    let arr = [];
+    const hashMap = {
+        "(": ")",
+        "[": "]",
+        "{": "}"
+    }
+    for(let i = 0; i < str.length; i++){
+        if(str[i] === "(" || str[i] === "[" || str[i] === "{"){
+            arr.push(str[i])
+        } else if (hashMap[arr.pop()] !== str[i]){
+            return "invalid"
+        }
+    }
+    return arr.length ? "invalid" : "valid"
+}
+isValid('{[()[]]()}()')
+
 
 module.exports = isValid;
--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -4,6 +4,27 @@
  * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
  * @returns {number} The decimal equivalent.
  */
-function romanToDecimal(roman) {}
+function romanToDecimal(roman) {
+    let result = 0
+    const hashMap = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    }
+    const arr = roman.split("");
+    for(let i = 0; i < arr.length; i++){
+        if(hashMap[arr[i]] < hashMap[arr[i + 1]]){
+            result -= hashMap[arr[i]]
+        } else {
+            result += hashMap[arr[i]]
+        }
+    }
+    return result
+}
+romanToDecimal("XI")
 
 module.exports = romanToDecimal;
