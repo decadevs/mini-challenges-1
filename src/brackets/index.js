04a21736ec3bc8a44bbf73276e1da478e65e8d9a
ereibor <etunere@gmail.com>
2023-01-30 00:04:01 +0100

    solved problems

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,45 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+// let isBalanced = (input) => {
+//   iterate through input
+// 	if the current element is an opening bracket, record the correct closing bracket
+//     if the current element is a closing bracket, check the record to make sure it is the correct closing bracket
+//   if all the opening and closing brackets are matched and in the correct order, return true, otherwise return false
+// }
+
+// function isValid(str) {
+// //Iterate through inputs
+
+// }
+
+
+function isValid(str){
+
+  let brackets = "[]{}()"
+  let stack = []
+
+  for(let bracket of str) {
+    let bracketsIndex = brackets.indexOf(bracket)
+
+    if (bracketsIndex === -1){
+      continue
+    }
+
+    if(bracketsIndex % 2 === 0) {
+      stack.push(bracketsIndex + 1)
+    } else {
+      if(stack.pop() !== bracketsIndex) {
+        return "invalid";
+      }
+    }
+  }
+  return stack.length === 0 ? "valid" : "invalid";
+}
+
+
+console.log(isValid("{[]"));
+
+
 
 module.exports = isValid;
