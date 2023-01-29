156793f005ebcf0ca43aac7637092e91de8fc321
Aksheg <123164721+Aksheg@users.noreply.github.com>
2023-01-29 23:11:10 +0100

    Aksheg

--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,22 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+    const rows = array.length, cols =array[0].length;
+const grid = [];
+for (let j = 0; j < cols; j++) {
+  grid[j] = Array(rows);
+}
+for (let i = 0; i < rows; i++) {
+  for (let j = 0; j < cols; j++) { 
+    grid[j][i] = array[i][j];
+  }
+}
+return grid;
+}
 
+console.log(transpose([
+    [1, 2, 3],
+    [3, 4, 5],
+]))
 module.exports = transpose;
