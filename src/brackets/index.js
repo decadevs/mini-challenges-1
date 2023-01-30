0c3da0e19d1630c5e3e987376f9b9a30c5a58132
Abidemi Omosanya <89324079+Bii-sama@users.noreply.github.com>
2023-01-30 13:00:20 +0100

    brackets

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,26 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+    let brackets = "[]{}()<>"
+    let stack = []
+    for(let bracket of str) { 
+        let bracketsIndex = brackets.indexOf(bracket) 
+        if (bracketsIndex === -1){ 
+            continue
+}
 
+if(bracketsIndex % 2 === 0) {
+     stack.push(bracketsIndex + 1) 
+    } else {
+        if(stack.pop() !== bracketsIndex) {
+             return false;
+            }
+            }
+}
+return stack.length === 0 ? "valid" : "invalid";
+}
 module.exports = isValid;
+
+
+
