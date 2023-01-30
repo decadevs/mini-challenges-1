a33e97409fffed136e8e11d44c8ee1e432216592
Teetony-rex <tolulope.ojo@decagon.dev>
2023-01-30 10:06:21 +0100

    Transpose-solution

--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,17 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+  let newArray = [];
+  //loop through the matrix and push empty arrays into newArray
+    for(let i = 0; i < array[0].length; i++){
+        newArray.push([]);
+// loop and push items into the empty arrays in the newArray
+        for(let j = 0; j < array.length; j++){
+            newArray[i].push(array[j][i]);
+        };
+    }return newArray;
+}
+
 
 module.exports = transpose;
