28cd012b6a95c3081c2df664d9977819bef6d8b1
Muhammad Mukhtar <yusoofjnr@gmail.com>
2023-01-30 15:30:15 +0100

    submitted

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,22 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+    
+ let stck = []
+
+ for(let i = 0; i < str.length; i++){
+        let char = stck[stck.length - 1]
+        if(str[i] == "(" || str[i] == "{" || str[i] == "[") {
+            stck.push(str[i]);
+
+        } else if ((char == "(" && str[i] == ")") || (char == "{" && str[i] == "}" || (char == "[" && str[i] == "]"))){
+            stck.pop();
+        }else{
+            return "invalid";
+        }
+ }
+return stck.length ? "invalid" : "valid";
+}
 
 module.exports = isValid;
