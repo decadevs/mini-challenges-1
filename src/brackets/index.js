f4c1fc24169ef79c1a9d5ef45f250fec73a1ef13
Abati Oluwabunmi <oluwabunmi.abati@decagon.dev>
2023-01-30 01:30:01 +0100

    Brackets: I wrote a fxn to check bracket order Roman: I wrote a function to get decimals Transpose: I wrote a function for matrix transpose

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,43 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+  let stack = [];
+
+
+  for (var i = 0; i < str.length; i++ ) {
+    let bracket = str[i];
+    if (bracket == "(" || bracket == "{" || bracket == "[") {
+      stack.push(bracket);
+      continue;
+      }
+    if (stack.length == 0) {
+        return false;
+    }
+
+    let confirmClose;
+    switch (bracket) {
+      case ")":
+          confirmClose = stack.pop();
+          if (confirmClose != "(")
+            return false;
+          break;
+
+      case "}":
+          confirmClose = stack.pop();
+          if (confirmClose != "{")
+            return false;
+          break;
+
+      case "]":
+          confirmClose = stack.pop();
+          if (confirmClose != "[")
+            return false;
+          break;
+
+    }
+  } return (stack.length == 0);
+}
+
 
 module.exports = isValid;
