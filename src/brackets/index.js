b08f455382d9f52029f03d949f31686e0c0c85da
nonsookongwu <60108641+nonsookongwu@users.noreply.github.com>
2023-01-30 11:36:17 +0100

    Solution to challenge one

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,33 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+  if(str.length % 2 === 1) return "invalid";
+  let main = []
+
+  for (let i = 0; i < str.length; i++){
+    let char = str[i]
+    if (char === "{" || char === "[" || char === "("){
+      main.push(char)
+      //console.log(main)
+
+    }else{
+      if(main.length === 0){
+        return "invalid"
+      }
+      const popped = main.pop()
+      if (char === "}" && popped.indexOf("{") === -1 || char === "]" && popped.indexOf("[") === -1 || char === ")" && popped.indexOf("(")=== -1){
+      return "invalid"
+    }
+    }
+    }
+    //console.log(main)
+
+    if (main.length !== 0){
+      return "invalid"
+    }else {
+      return "valid"
+    }
+}
 
 module.exports = isValid;
--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,20 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+  let main = []
+    for (let i = 0; i < array[0].length; i++){
+      //console.log(main)
+      main.push([])
+      //console.log(main)
+
+      for(let j = 0; j < array.length; j++){
+
+        main[i].push(array[j][i])
+        //console.log(array[j][i])
+      }
+    }
+    return main
+}
 
 module.exports = transpose;
