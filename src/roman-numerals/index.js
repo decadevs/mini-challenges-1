ed35c61a91964a6e69e579d64a992fe7109479a4
Decagon <decagon@Decagons-MacBook-Air.local>
2023-01-30 13:56:10 +0100

    week1_task2

--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -4,6 +4,39 @@
  * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
  * @returns {number} The decimal equivalent.
  */
-function romanToDecimal(roman) {}
+
+//function called "romanToDecimal" which takes in a single input,
+//a string "roman" which represents a Roman numeral.
+function romanToDecimal(roman) {
+    //creation of an object called "rValues" that maps each Roman numeral
+    //character to its corresponding integer value.  
+    const rValues = {
+      I:1,
+      V:5,
+      X:10,
+      L:50,
+      C:100,
+      D:500,
+      M:1000
+     };
+     //Initialization of a variable called "total" which is initially set to 0.
+     //This variable will be used to store the final integer value of the Roman numeral.
+     let total = 0;
+     //A for loop to iterate through each character in the input string.
+    for(let i = 0; i < roman.length; i++){
+        //If the current roman numeral character is less than the next roman numeral character,
+        //the current roman numeral character will be subtracted from the next roman numeral 
+        //character, and vice versa.
+        if(rValues[roman[i]] < (rValues[roman[i+1]])){
+         total += ((rValues[roman[i+1]]) - rValues[roman[i]]);
+         i++;
+        } else {
+          total += rValues[roman[i]];
+        }
+    }
+    return total;
+  };
+  console.log(romanToDecimal("XL"));
+  console.log(romanToDecimal("LX"));
 
 module.exports = romanToDecimal;
--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,21 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+    //Initialized as an empty array.
+    let transposed = [];
+    //a FOR loop that will iterate i over the length of the
+    //first element of array. This is the number of columns in the array.
+    for (let i = 0; i < array[0].length; i++){
+        //pushing an empty array into transposed, this will be used to store a row of the transposed array.
+      transposed.push([]);
+      //A nested loop that will iterate j over the length of array, which is the number of rows in the array.
+      for (let j = 0; j < array.length; j++){
+        //transposed[i].push([j][i]) pushes the value of array[j][i] into transposed[i], effectively transposing the array.
+        transposed[i].push(array[j][i]);
+      }
+    }
+    return transposed;
+  }
 
 module.exports = transpose;
