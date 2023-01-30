b2b49e17d752fac6548fc7df43127f930ad07a0e
Abidemi Omosanya <89324079+Bii-sama@users.noreply.github.com>
2023-01-30 02:06:47 +0100

    semi-solved

--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,27 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+
+    let newArr = []
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array[0].length; j++) {
+        newArr.push(array[i][j])
+        
+    }
+    
+  }
+return newArr
+}
+
+let a = [
+    [9, 1, 2, 3],
+    [4, 5, 6, 7],
+    [8, 9, 10, 7],
+    [1, 3, 1, 5],
+  ]
+
+
+  console.log(transpose(a))
 
 module.exports = transpose;
