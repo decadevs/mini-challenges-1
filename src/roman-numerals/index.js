cae44fbaf6eeebac53b1f0375c9a928428947523
Victor Okoroafor <victor.okoroafor@decagon.dev>
2023-01-30 11:17:45 +0100

    master

--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -4,6 +4,27 @@
  * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
  * @returns {number} The decimal equivalent.
  */
-function romanToDecimal(roman) {}
+function romanToDecimal(roman) {
+     let map = {
+      I: 1,
+      V: 5,
+      X: 10,
+      L: 50,
+      C: 100,
+      D: 500,
+      M: 1000
+    };
+    let result = 0;
+    for (let i = 0; i < roman.length; i++) {
+      let current = map[roman[i] ];
+      let next = map[ roman[i + 1] ] || 0;
+      if (current < next) {
+        result -= current;
+      } else {
+        result += current;
+      }
+    }
+    return result;
+}
 
 module.exports = romanToDecimal;
--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,15 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+    let transposed = [];
+  for (let i = 0; i < arr[0].length; i++) {
+    transposed.push([]);
+    for (let j = 0; j < arr.length; j++) {
+      transposed[i].push(arr[j][i]);
+    }
+  }
+  return transposed;
+}
 
 module.exports = transpose;
