0f31a95058e5ab5ea37f8941a5dda49fc6c5b12f
smartfusion01 <itsealonge@yahoo.com>
2023-01-30 15:21:43 +0100

    Transpose and Roman Numerals

--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -4,6 +4,30 @@
  * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
  * @returns {number} The decimal equivalent.
  */
-function romanToDecimal(roman) {}
+function romanToDecimal(roman) {
+  let sum = 0;
+  let romanNum = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000
+  };
+
+
+for (let i = 0; i < roman.length; i++){
+
+
+  if (romanNum[roman[i]] < romanNum[roman[i+1]]){
+
+    sum = sum - romanNum[roman[i]];
+  } else {
+    sum = sum + romanNum[roman[i]];
+  }
+}
+return sum;
+}
 
 module.exports = romanToDecimal;
--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,17 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+  let result = [];
+
+  for (let i = 0; i < array[0].length; i++){
+    let temp = [];
+    for(let j = 0; j < array.length; j++){
+      temp.push(array[j][i]);
+    }
+    result.push(temp);
+  }
+ return result;
+}
 
 module.exports = transpose;
