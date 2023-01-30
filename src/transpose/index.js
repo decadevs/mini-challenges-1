41724ce0fc54bf756457f4d86be40e4afdd5edfb
Damilola Adegoke <d.botanist20@gmail.com>
2023-01-30 10:00:51 +0100

    solved problems

--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,20 +4,6 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {
-    const rows = array.length, cols =array[0].length;
-    const grid = [];
-    for (let j = 0; j < cols; j++) {
-        grid[j] = Array(rows);
-    }
-    for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < cols; j++) {
-            grid[j][i] = array[i][j];
-        }
-    }
-    return grid;
-}
-    console.log(transpose([[1, 2, 3],[3, 4, 5],]))
-
+function transpose(array) {}
 
 module.exports = transpose;
