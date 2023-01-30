eb73254c4a30f67222902c991939e8f86c5f0e1d
Ediomo Titus <ediomo.titus@decagon.dev>
2023-01-30 12:29:45 +0100

    submit challenge one

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,43 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+  if (str.length % 2 !== 0) {
+    return "invalid";
+  }
+  let openBraces = [];
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "(" || str[i] === "{" || str[i] === "[") {
+      openBraces.push(str[i]);
+    } else {
+      let braces = openBraces.pop();
+      if (
+        !(
+          (str[i] === ")" && braces === "(") ||
+          (str[i] === "}" && braces === "{") ||
+          (str[i] === "]" && braces === "[")
+        )
+      ) {
+        return "invalid";
+      }
+    }
+  }
+  if (openBraces.length === 0) {
+    return "valid";
+  } else {
+    return "invalid";
+  }
+}
+
+console.log(isValid("(}"));
+console.log(isValid("{[()[]]()}()"));
+console.log(isValid("{{{(())}}[[()]]}"));
+console.log(isValid("[{()}({[]})(){}[]]"));
+console.log(isValid("(){}[](([][]){})"));
+console.log(isValid("{{[][()]}{[][()]}{[][()]}}"));
+console.log(isValid("({()}[()]}"));
+console.log(isValid("[](){{{}}"));
+console.log(isValid("{{{{{{{{{{{{}}}}}}}}}}}()[]"));
+console.log(isValid("}{{{{}}}}{([])(((()))){"));
 
 module.exports = isValid;
--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -4,6 +4,26 @@
  * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
  * @returns {number} The decimal equivalent.
  */
-function romanToDecimal(roman) {}
+function romanToDecimal(roman) {
+    const romanValues = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    };
+    let decimal = 0;
+    for (let i = 0; i < roman.length; i++) {
+        if (romanValues[roman[i]] < romanValues[roman[i + 1]]) {
+            decimal -= romanValues[roman[i]];
+        } else {
+            decimal += romanValues[roman[i]];
+        }
+    }
+    return decimal;
+}
+
 
 module.exports = romanToDecimal;
