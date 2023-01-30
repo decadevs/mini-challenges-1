0737ea88277212c836645466154966a8cc594d72
Olatunji Clement <olatunjibolaji900@gmail.com>
2023-01-30 14:30:59 +0100

    *New commit

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,9 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+    while(/\(\)|\[\]|\{\}/g.test(braces)){braces = braces.replace(/\(\)|\[\]|\{\}/g,"")}
+    return !braces.length;
+}
 
 module.exports = isValid;
--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,10 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+    return array[0].map((_, i) => array.map(row => row[i]));
+}
+console.log(transpose([[9, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 7], [1, 3, 1, 5]])); 
+
 
 module.exports = transpose;
