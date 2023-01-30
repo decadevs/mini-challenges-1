ba616bdf2178bc3a7bdcbb34e79db284eba1fa40
Teetony-rex <tolulope.ojo@decagon.dev>
2023-01-30 09:58:55 +0100

    isValid algorithm solution

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,26 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+//assigning all bracket into a variable brackets and assign an empty array to newBracket
+    let bracket = "[]{}()"
+    let newBracket =[]
+//loop through str using the for-of loop
+    for (let item of str){
+      let bracketIndex = bracket.indexOf(item)
+// edge case
+      if (bracketIndex === -1){
+        continue
+// push even indexes into new bracket
+      }if(bracketIndex%2 === 0){
+        newBracket.push(bracketIndex+1)
+      }else{
+        if(newBracket.pop() !== bracketIndex){
+          return "invalid"
+        }
+      }
+    }return  newBracket.length === 0?"valid":"invalid";
+
+}
 
 module.exports = isValid;
