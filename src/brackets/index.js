1802d2f8b8f867de8a87c3caa99416acc2fb93c3
mikekinjobi <mikekinjobi@gmail.com>
2023-05-10 09:26:53 +0100

    Answers to Mini-Challenges

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,29 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+    let brackets=[
+        {open: '{', close: '}'}, 
+        {open: '[', close:']'}, 
+        {open: '(', close: ')'}
+        ]
+        
+        let checkstr=[];
+        str=str.split('');
+        for(let i=0; i<str.length; i++){
+            for(let j=0; j<brackets.length; j++){
+                if(str[i]===brackets[j].open){
+                    checkstr.push(brackets[j].open)
+                    break;
+                }else if(str[i]===brackets[j].close){
+                    if(brackets[j].open !== checkstr.pop())
+                    return "invalid";
+                }
+            }
+        }
+        
+        
+        return "valid";
+}
 
 module.exports = isValid;
--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -4,6 +4,34 @@
  * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
  * @returns {number} The decimal equivalent.
  */
-function romanToDecimal(roman) {}
+function romanToDecimal(roman) {
+    let obj = { 
+        "M": 1000,
+        "D": 500,
+        "C": 100,
+        "L": 50,
+        "X": 10,
+        "V": 5,
+        "I": 1 
+        }
+        
+        let sum = 0;
+        
+        roman= roman.split('');
+        
+        for(let i=0; i<roman.length; i++){
+        if(i+1<roman.length){
+            if(obj[roman[i]] < obj[roman[i+1]]){
+                sum-=obj[roman[i]];
+            }else{
+                sum+=obj[roman[i]]
+            }
+        }else{
+            sum+=obj[roman[i]];
+        }
+        
+    }
+        return sum;
+}
 
 module.exports = romanToDecimal;
