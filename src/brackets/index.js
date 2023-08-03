129788c68228be3a06993b963779f724f3f551da
mac <mac@macs-MacBook-Air-2.local>
2023-08-03 21:54:48 +0100

    test

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,28 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+    let bracket = [];
+    let opening = '({[';
+    let closing = ')}]';
+    let matching = {
+      ')': '(',
+      '}': '{',
+      ']': '[',
+    };
+  
+for(let i =0; i <str.length; i++) { 
+      if (opening.includes(str[i])) {
+        bracket.push(str[i]);
+      } else if (closing.includes(str[i])) {
+        const lastBracket = bracket.pop();
+        if (matching[str[i]] !== lastBracket) {
+          return 'invalid';
+        }
+      }
+    }
+  
+    return bracket.length === 0 ? 'valid' : 'invalid';
+}
 
 module.exports = isValid;
