f16cd04a3ca1e3e6244cabe5a493c1e176b96805
ANYIDOLLAR <anyidollar@wikiwoos-MacBook-Air.local>
2023-08-07 21:33:26 +0100

    this is my first mini-challenge

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,35 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+
+function isValid(str) {
+    let stack = []
+    for(let i in str) {
+        let x = stack.length - 1
+        if(str[i]=="(" || str[i]=="{" || str[i]=="[") {
+            stack.push(str[i])
+        }
+        else{
+            if(str[i]== ")" && stack[x]=="(") {
+                stack.pop()
+            }
+            else if(str[i]=="}" && stack[x]=="{") {
+                stack.pop()
+            }
+            else if(str[i]=="]" && stack[x]=="[") {
+                stack.pop()
+            }
+            else{
+                return "invalid"
+            }
+        }
+    }
+    return stack.length > 0 ? "invalid" : "valid"
+}
 
 module.exports = isValid;
+
+
+
+
+
