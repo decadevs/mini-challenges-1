964c6dc602e9821dfa33d73379bced241ab762be
John <mac@MACs-MacBook-Air.local>
2023-08-07 23:52:08 +0100

    my solution to the mini challenges

--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,7 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
-
+function transpose(array) {
+  return array[0].map((item, index) => array.map((cur) => cur[index]))
+}
 module.exports = transpose;
