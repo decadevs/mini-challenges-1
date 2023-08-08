d0189a5dff312a1ebc6af0fc24a87d84235e1ac1
ebuxtain <100353286+Ebuxtain@users.noreply.github.com>
2023-08-09 00:39:57 +0100

    my new task

--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,18 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+let array = [[5, 12, 17, 9, 3], [13, 4, 8, 14, 1], [9, 6, 3, 7, 21]]
+function transpose(array) {
+    let transposedArr = [];
+
+    for ( let i = 0; i < array[0].length; i++){
+        transposedArr [i]= [];
+        for (let j =0; j <  array.length; j++){
+            transposedArr[i][j] = array[j][i]
+        }
+    }
+
+    return transposedArr
+}
 
 module.exports = transpose;
