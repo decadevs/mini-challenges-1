4a6e21dafb7785bb0f373e20d3286a4db762a594
Pgodwin1 <pgodwin287@gmail.com>
2023-08-08 14:38:05 +0100

    my first commit

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,23 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+    const stack = [];
+    const open = "({[";
+    const close = ")}]";
+
+    for(let char of str) {
+        if(open.includes(char)) {
+            stack.push(char);
+        } else if(close.includes(char)) {
+            const lastOpenBracket = stack.pop();
+            if(!lastOpenBracket || open.indexOf(lastOpenBracket) !== close.indexOf(char)) {
+                return "invalid";
+            }
+        }  
+    }
+    return stack.length === 0 ? "valid" : "invalid";
+}
+
 
 module.exports = isValid;
