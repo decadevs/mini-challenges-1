41b9a090defd27572131bd3ccdb759b3976a1827
MAC <mac@Gideons-MacBook-Air.local>
2023-08-09 00:08:25 +0100

    my mini-challenge

--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -4,6 +4,29 @@
  * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
  * @returns {number} The decimal equivalent.
  */
-function romanToDecimal(roman) {}
+function romanToDecimal(roman) {
+    const values = {
+        I : 1,
+        V : 5,
+        X : 10,
+        L : 50,
+        C : 100,
+        D : 500,
+        M : 1000,
+    }
+    let result = 0;
+    let previousChar = 0;
+    for (let i = roman.length -1; i >= 0; i--){
+        const currentChar = values[roman[i]];
+        if (currentChar < previousChar){
+            result -= currentChar;
+        } else {
+            result += currentChar;
+        }
+        previousChar = currentChar
+    }
+    return result
 
+}
+  
 module.exports = romanToDecimal;
