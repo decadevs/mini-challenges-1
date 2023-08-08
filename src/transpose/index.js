6be6f10fc486387817d626478d2946e7de7f1968
Harney Hart <mac@Harneys-MacBook.local>
2023-08-08 21:02:56 +0100

    fix functions within mini-challenges

--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,24 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+
+        //  create new Parent and Child Arrays
+        const newRowLength = array.length;
+        const newColLength = array[0].length;
+      
+        const transposedArray = [];
+      
+        // iterate over array to create new rows and column
+        for (let col = 0; col < newColLength; col++) {
+          const newRow = [];
+          for (let row = 0; row < newRowLength; row++) {
+            newRow.push(array[row][col]);
+          }
+          transposedArray.push(newRow);
+        }
+      
+    return transposedArray;
+}
 
 module.exports = transpose;
