b3be1ed37ff3010625d2e5eaf2ad73ce56229baf
Kasie97 <66069819+Kasie97@users.noreply.github.com>
2023-08-08 21:11:44 +0100

    DId the Assignments in this work

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,28 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+    const validBrackets = [];
+    const brackets = {
+        "(" : ")",
+        "{" : "}",
+        "[" : "]"
+    }
+    for (let i = 0; i < str.length; i++) {
+        const result = str[i];
+    }
+    if (brackets[result] !== undefined) {
+        validBrackets.push(result);
+    } else {  
+        let endingBracket = validBrackets.pop();
+        let correctClosingBracket = brackets[endingBracket];
+        if (result !== correctClosingBracket) {
+            return invalid;
+        }
+    }
+
+    return validBrackets.length === 0;
+
+}
 
 module.exports = isValid;
--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -4,6 +4,25 @@
  * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
  * @returns {number} The decimal equivalent.
  */
-function romanToDecimal(roman) {}
+function romanToDecimal(roman) {
+    const ROMANNUMERALS = {
+        "I": 1,"V": 5,"X": 10,"L": 50,"C": 100,"D": 500,"M": 1000 };
+    
+      let decimalNumbers = 0;
+    
+      for (let i = 0; i < ROMANNUMERALS.length; i++) {
+        const firstLetter = ROMANNUMERALS[ROMANNUMERALS[i]];
+        const nextLetter = ROMANNUMERALS[ROMANNUMERALS[i + 1]];
+    
+        if (nextLetter && nextLetter > firstLetter) {
+          decimalNumbers += nextLetter - firstLetter;
+          i++; 
+        } else {
+          decimalNumbers += firstLetter;
+        }
+      }
+    
+      return decimalNumbers;
+}
 
 module.exports = romanToDecimal;
