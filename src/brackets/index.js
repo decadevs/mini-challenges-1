277a3ea904fe476a4c1ca528781fc04ba1a98f38
Rickyfreaky <139926675+Rickyfreaky@users.noreply.github.com>
2023-08-08 15:00:50 +0100

    this is my first commit

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,34 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str){
+    let pack = []
+    for (let i=0; i<str.length;i++){
+        let x = pack.length-1
+        if(str[i] =='(' || str[i]=='[' || str[i]=='{'){
+            pack.push(str[i])
+        }
+        else{
+        if(str[i]==')' && pack[x]=='('){
+            pack.pop()
+        }
+        else if (str[i]=='}' && pack[x]== '{'){
+            pack.pop()
+        }
+        else if(str[i]== ']' && pack[x]=='['){
+            pack.pop()
+
+        }
+        else {
+            return 'invalid'
+        }
+    }
+
+
+    }
+    return pack.length > 0 ? 'invalid' : 'valid'
+}
 
 module.exports = isValid;
+
+
--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -4,6 +4,53 @@
  * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
  * @returns {number} The decimal equivalent.
  */
-function romanToDecimal(roman) {}
+function romanToDecimal(roman) {
+     
+        roman = roman.toUpperCase();
+        let decimalValue = 0;
+        
+        for (let i = 0; i < roman.length; i++) {
+            switch (roman[i]) {
+                case 'M':
+                    decimalValue += 1000;
+                    break;
+                case 'D':
+                    decimalValue += 500;
+                    break;
+                case 'C':
+                    if (roman[i + 1] === 'D' || roman[i + 1] === 'M') {
+                        decimalValue -= 100;
+                    } else {
+                        decimalValue += 100;
+                    }
+                    break;
+                case 'L':
+                    decimalValue += 50;
+                    break;
+                case 'X':
+                    if (roman[i + 1] === 'L' || roman[i + 1] === 'C') {
+                        decimalValue -= 10;
+                    } else {
+                        decimalValue += 10;
+                    }
+                    break;
+                case 'V':
+                    decimalValue += 5;
+                    break;
+                case 'I':
+                    if (roman[i + 1] === 'V' || roman[i + 1] === 'X') {
+                        decimalValue -= 1;
+                    } else {
+                        decimalValue += 1;
+                    }
+                    break;
+            }
+        }
+        
+        return decimalValue;
+    }
+    
 
+    
+    
 module.exports = romanToDecimal;
