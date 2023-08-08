e961fdaa8bcc33b038d803fbece24ab7f88e972f
mac <pro@Pros-MacBook-Pro.local>
2023-08-08 22:39:17 +0100

    Copleted mini-challenge-1

--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,15 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
-
+function transpose(array) {
+    let transposedArr = [];
+    for (let i = 0; i < array[0].length; i++) {
+        let arrChild = [];
+        for (let j = 0; j < array.length; j++) {
+            arrChild.push(array[j][i]);
+        }
+        transposedArr.push(arrChild);
+    }
+    return(transposedArr);
+}
 module.exports = transpose;
