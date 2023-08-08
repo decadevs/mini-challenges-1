9efaabbb2e816b4ea5a0e85eaa70a2e681925e79
Vhal <mac@Vhals-MacBook-Air-3.local>
2023-08-08 20:02:59 +0100

    my first decagon task submission

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,42 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+
+
+
+        
+function isValid(str) {
+    
+    const stack = [];
+    let bracketsString = {
+        '(':')',
+        '{':'}',
+        '[':']',
+    };   
+
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i];
+        if (bracketsString[char]) {
+            stack.push(char);
+        } else if (char == ')' || char == '}' || char == ']') {
+            let lastBracket = stack.pop();
+        
+            if (bracketsString[lastBracket] != char) {
+                return "invalid";
+                
+            }
+        }
+    }
+
+   return stack.length == 0 ? "valid" : "invalid"
+    }
+
+    // // Test cases
+    // const testStrings = //"({()}[()]}";
+    // "{{{()[]" ;
+    //console.log(isValid(testStrings))
+    
+    
+
 
 module.exports = isValid;
--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,17 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
 
+let numberArr = [
+    [5, 12, 17, 9, 3],
+    [13, 4, 8, 14, 1],
+    [9, 6, 3, 7, 21],
+  ];
+
+let newArr = transpose(numberArr);
+
+function transpose(array) {
+return array[0].map((col, i) => array.map(row =>row[i]));
+}
+console.log(newArr)
 module.exports = transpose;
